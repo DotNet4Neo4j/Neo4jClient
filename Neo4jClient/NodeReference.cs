959aa711570c4e79afbd58bcb5a458760fb6feaf
Tatham Oddie <tatham@oddie.com.au>
2011-06-09 22:38:05 +1000

    Added custom equality handling to NodeReference.

--- Neo4jClient/NodeReference.cs
+++ Neo4jClient/NodeReference.cs
@@ -17,5 +17,28 @@
         {
             return new NodeReference(nodeId);
         }
+
+        public static bool operator ==(NodeReference lhs, NodeReference rhs)
+        {
+            return lhs.Equals(rhs);
+        }
+
+        public static bool operator !=(NodeReference lhs, NodeReference rhs)
+        {
+            return !(lhs == rhs);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as NodeReference;
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return other.id == id;
+        }
+
+        public override int GetHashCode()
+        {
+            return id;
+        }
     }
 }
--- Test/NodeReferenceTests.cs
+++ Test/NodeReferenceTests.cs
@@ -19,5 +19,59 @@ namespace Neo4jClient.Test
             Assert.IsInstanceOf(typeof(NodeReference), nodeReference);
             Assert.AreEqual(3, nodeReference.Id);
         }
+
+        [Test]
+        [TestCase(1, 2, Result = false)]
+        [TestCase(3, 3, Result = true)]
+        public bool Equals(int lhs, int rhs)
+        {
+            return new NodeReference(lhs) == new NodeReference(rhs);
+        }
+
+        [Test]
+        [TestCase(1, 2, Result = false)]
+        [TestCase(3, 3, Result = true)]
+        public bool GetHashCode(int lhs, int rhs)
+        {
+            return new NodeReference(lhs).GetHashCode() == new NodeReference(rhs).GetHashCode();
+        }
+
+        [Test]
+        public void EqualsShouldReturnFalseWhenComparingWithNull()
+        {
+            var lhs = new NodeReference(3);
+            Assert.IsFalse(lhs.Equals(null));
+        }
+
+        [Test]
+        public void EqualsShouldReturnFalseWhenComparingWithDifferentType()
+        {
+            var lhs = new NodeReference(3);
+            Assert.IsFalse(lhs.Equals(new object()));
+        }
+
+        [Test]
+        public void EqualsShouldReturnTrueWhenComparingRootNodeWithZero()
+        {
+            var lhs = NodeReference.RootNode;
+            var rhs = new NodeReference(0);
+            Assert.IsTrue(lhs == rhs);
+        }
+
+        [Test]
+        public void EqualsShouldReturnTrueWhenComparingRootNodeWithRootNode()
+        {
+            var lhs = NodeReference.RootNode;
+            var rhs = NodeReference.RootNode;
+            Assert.IsTrue(lhs == rhs);
+        }
+
+        [Test]
+        public void EqualsShouldReturnFalseWhenComparingRootNodeWithNonZero()
+        {
+            var lhs = NodeReference.RootNode;
+            var rhs = new NodeReference(4);
+            Assert.IsFalse(lhs == rhs);
+        }
     }
 }
