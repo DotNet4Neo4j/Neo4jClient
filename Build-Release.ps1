4b549d24c71e2c8d25df0f662c6070e471e221a7
Tatham Oddie <tatham@oddie.com.au>
2011-06-09 11:22:04 +1000

    Added release build script.

---
+++ Build-Release.ps1
@@ -0,0 +1,52 @@
+﻿param (
+	[Parameter(Mandatory=$true)]
+	[ValidatePattern("\d\.\d\.\d\.\d")]
+	[string]
+	$ReleaseVersionNumber
+)
+
+$ErrorActionPreference = "Stop"
+
+$PSScriptFilePath = (Get-Item $MyInvocation.MyCommand.Path).FullName
+
+$SolutionRoot = Split-Path -Path $PSScriptFilePath -Parent
+
+$Is64BitSystem = (Get-WmiObject -Class Win32_OperatingSystem).OsArchitecture -eq "64-bit"
+$Is64BitProcess = [IntPtr]::Size -eq 8
+
+$RegistryArchitecturePath = ""
+if ($Is64BitProcess) { $RegistryArchitecturePath = "\Wow6432Node" }
+
+$FrameworkArchitecturePath = ""
+if ($Is64BitSystem) { $FrameworkArchitecturePath = "64" }
+
+$ClrVersion = (Get-ItemProperty -path "HKLM:\SOFTWARE$RegistryArchitecturePath\Microsoft\VisualStudio\10.0")."CLR Version"
+
+$MSBuild = "$Env:SYSTEMROOT\Microsoft.NET\Framework$FrameworkArchitecturePath\$ClrVersion\MSBuild.exe"
+
+# Set the version number in AssemblyInfo.cs
+$AssemblyInfoPath = Join-Path -Path $SolutionRoot -ChildPath "Neo4jClient\Properties\AssemblyInfo.cs"
+(gc -Path $AssemblyInfoPath) `
+	-replace "(?<=Version\(`")[.\d]*(?=`"\))", $ReleaseVersionNumber |
+	sc -Path $AssemblyInfoPath -Encoding UTF8
+
+# Build the solution in release mode
+$SolutionPath = Join-Path -Path $SolutionRoot -ChildPath "Neo4jClient.sln"
+& $MSBuild "$SolutionPath" /p:Configuration=Release /maxcpucount /t:Clean
+if (-not $?)
+{
+	throw "The MSBuild process returned an error code."
+}
+& $MSBuild "$SolutionPath" /p:Configuration=Release /maxcpucount
+if (-not $?)
+{
+	throw "The MSBuild process returned an error code."
+}
+
+# Build the NuGet package
+$NuSpecPath = Join-Path -Path $SolutionRoot -ChildPath "Neo4jClient\Neo4jClient.nuspec"
+& nuget pack $NuSpecPath -OutputDirectory $SolutionRoot -Version $ReleaseVersionNumber
+if (-not $?)
+{
+	throw "The NuGet process returned an error code."
+}
