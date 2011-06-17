2f368a89e69786d5c4f1da76efc8b770a3638971
Tatham Oddie <tatham@oddie.com.au>
2011-06-17 11:13:32 +1000

    Removed Connect method from IGraphClient as it's an implementation specific concept.

--- Neo4jClient/IGraphClient.cs
+++ Neo4jClient/IGraphClient.cs
@@ -5,7 +5,6 @@ namespace Neo4jClient
 {
     public interface IGraphClient
     {
-        void Connect();
         NodeReference<TNode> Create<TNode>(TNode node, params IRelationshipAllowingParticipantNode<TNode>[] relationships) where TNode : class;
         TNode Get<TNode>(NodeReference reference);
         TNode Get<TNode>(NodeReference<TNode> reference);
