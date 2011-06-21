802bf2b74f2e391dc5368fdcdaa68a01094d9ca5
Romiko Derbynew <romiko.derbynew@readify.net>
2011-06-21 11:55:55 +1000

    Added empty unit test case to test serialization of null property values.

---
+++ Test/RestSharpTests/RestClientTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Neo4jClient.Test.RestSharpTests
+{
+    [TestFixture]
+    class RestClientTests
+    {
+        [Test]
+        public void ExecuteShouldNotJSonSerializeNullProperties()
+        {
+            Assert.Inconclusive("To be Implemented.");
+        }
+    }
+}
