47876a5cba2ef42c6390c81aee2c2ca9802a07ab
Tatham Oddie <tatham@oddie.com.au>
2011-06-23 09:22:12 +1000

    Added new Node<TNode> class.

---
+++ Neo4jClient/Node`TNode.cs
@@ -0,0 +1,24 @@
+﻿namespace Neo4jClient
+{
+    public class Node<TNode>
+    {
+        readonly TNode data;
+        readonly NodeReference<TNode> reference;
+
+        public Node(TNode data, NodeReference<TNode> reference)
+        {
+            this.data = data;
+            this.reference = reference;
+        }
+
+        public NodeReference<TNode> Reference
+        {
+            get { return reference; }
+        }
+
+        public TNode Data
+        {
+            get { return data; }
+        }
+    }
+}
