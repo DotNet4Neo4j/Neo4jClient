685bd93905fd13ab088a0f5ae5d217c42d7f0335
Tatham Oddie <tatham@oddie.com.au>
2011-06-23 19:46:04 +1000

    Added tests for OutV.

---
+++ Test/Gremlin/BasicStepsTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using Neo4jClient.Gremlin;
+
+namespace Neo4jClient.Test.Gremlin
+{
+    [TestFixture]
+    public class BasicStepsTests
+    {
+        [Test]
+        public void OutVShouldAppendStepToNodeReference()
+        {
+            var node = new NodeReference(123);
+            var queryText = node.OutV().QueryText;
+            Assert.AreEqual("g.v(123).outV", queryText);
+        }
+
+        [Test]
+        public void OutVShouldAppendStepToGremlinQuery()
+        {
+            var node = new NodeReference(123);
+            var queryText = node.OutV().OutV().QueryText;
+            Assert.AreEqual("g.v(123).outV.outV", queryText);
+        }
+    }
+}
