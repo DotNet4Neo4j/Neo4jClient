18de7df0293eb5de05945a2c8a407102f421595a
Tatham Oddie <tatham@oddie.com.au>
2011-06-23 22:13:23 +1000

    Added support for InV with filters, OutV with filters and Count.

--- Neo4jClient/Gremlin/BasicSteps.cs
+++ Neo4jClient/Gremlin/BasicSteps.cs
@@ -1,4 +1,9 @@
-﻿namespace Neo4jClient.Gremlin
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace Neo4jClient.Gremlin
 {
     public static class BasicSteps
     {
@@ -8,9 +13,31 @@
             return new GremlinNodeEnumerable<TNode>(query.Client, queryText);
         }
 
+        public static IGremlinNodeQuery<TNode> OutV<TNode>(this IGremlinQuery query, NameValueCollection filters)
+        {
+            var formattedFilters = filters
+                .AllKeys
+                .Select(k => new KeyValuePair<string, string>(k, filters[k]))
+                .Select(f => string.Format("['{0}':'{1}']", f.Key, f.Value))
+                .ToArray();
+            var queryText = string.Format("{0}.outV[{1}]", query.QueryText, string.Join(",", formattedFilters));
+            return new GremlinNodeEnumerable<TNode>(query.Client, queryText);
+        }
+
         public static IGremlinNodeQuery<TNode> InV<TNode>(this IGremlinQuery query)
         {
             var queryText = string.Format("{0}.inV", query.QueryText);
+            return new GremlinNodeEnumerable<TNode>(query.Client, queryText);
+        }
+
+        public static IGremlinNodeQuery<TNode> InV<TNode>(this IGremlinQuery query, NameValueCollection filters)
+        {
+            var formattedFilters = filters
+                .AllKeys
+                .Select(k => new KeyValuePair<string, string>(k, filters[k]))
+                .Select(f => string.Format("['{0}':'{1}']", f.Key, f.Value))
+                .ToArray();
+            var queryText = string.Format("{0}.inV[{1}]", query.QueryText, string.Join(",", formattedFilters));
             return new GremlinNodeEnumerable<TNode>(query.Client, queryText);
         }
 
@@ -36,6 +63,20 @@
         {
             var queryText = string.Format("{0}.inE[[label:'{1}']]", query.QueryText, label);
             return new GremlinReferenceEnumerable(query.Client, queryText);
+        }
+
+        public static int Count(this IGremlinQuery query)
+        {
+            var queryText = string.Format("{0}.count()", query.QueryText);
+            var scalarResult = query.Client.ExecuteScalarGremlin(queryText, new NameValueCollection());
+
+            int result;
+            if (!int.TryParse(scalarResult, out result))
+                throw new ApplicationException(string.Format(
+                    "Query returned an unexpected value. Expected an integer. Received: {0}",
+                    scalarResult));
+
+            return result;
         }
     }
 }
--- Test/Gremlin/BasicStepsTests.cs
+++ Test/Gremlin/BasicStepsTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Specialized;
+using NSubstitute;
+using NUnit.Framework;
 using Neo4jClient.Gremlin;
 
 namespace Neo4jClient.Test.Gremlin
@@ -23,6 +25,33 @@ namespace Neo4jClient.Test.Gremlin
         }
 
         [Test]
+        public void OutVShouldAppendStepToGremlinQueryWithSingleFilter()
+        {
+            var node = new NodeReference(123);
+            var queryText = node
+                .OutV<object>(new NameValueCollection
+                {
+                    { "Foo", "Bar" }
+                })
+                .QueryText;
+            Assert.AreEqual("g.v(123).outV[['Foo':'Bar']]", queryText);
+        }
+
+        [Test]
+        public void OutVShouldAppendStepToGremlinQueryWithTwoFilters()
+        {
+            var node = new NodeReference(123);
+            var queryText = node
+                .OutV<object>(new NameValueCollection
+                {
+                    { "Foo", "Bar" },
+                    { "Baz", "Qak" }
+                })
+                .QueryText;
+            Assert.AreEqual("g.v(123).outV[['Foo':'Bar'],['Baz':'Qak']]", queryText);
+        }
+
+        [Test]
         public void OutVShouldReturnTypedGremlinEnumerable()
         {
             var node = new NodeReference(123);
@@ -44,6 +73,33 @@ namespace Neo4jClient.Test.Gremlin
             var node = new NodeReference(123);
             var queryText = node.InV<object>().QueryText;
             Assert.AreEqual("g.v(123).inV", queryText);
+        }
+
+        [Test]
+        public void InVShouldAppendStepToGremlinQueryWithSingleFilter()
+        {
+            var node = new NodeReference(123);
+            var queryText = node
+                .InV<object>(new NameValueCollection
+                {
+                    { "Foo", "Bar" }
+                })
+                .QueryText;
+            Assert.AreEqual("g.v(123).inV[['Foo':'Bar']]", queryText);
+        }
+
+        [Test]
+        public void InVShouldAppendStepToGremlinQueryWithTwoFilters()
+        {
+            var node = new NodeReference(123);
+            var queryText = node
+                .InV<object>(new NameValueCollection
+                {
+                    { "Foo", "Bar" },
+                    { "Baz", "Qak" }
+                })
+                .QueryText;
+            Assert.AreEqual("g.v(123).inV[['Foo':'Bar'],['Baz':'Qak']]", queryText);
         }
 
         [Test]
@@ -100,11 +156,23 @@ namespace Neo4jClient.Test.Gremlin
             var queryText = NodeReference
                 .RootNode
                 .OutE("E_FOO")
-                .InV<Foo>()
+                .InV<Foo>(new NameValueCollection { { "Foo", "Bar" } })
                 .InE("E_BAR")
                 .InV<Bar>()
                 .QueryText;
-            Assert.AreEqual("g.v(0).outE[[label:'E_FOO']].inV.inE[[label:'E_BAR']].inV", queryText);
+            Assert.AreEqual("g.v(0).outE[[label:'E_FOO']].inV[['Foo':'Bar']].inE[[label:'E_BAR']].inV", queryText);
+        }
+
+        [Test]
+        public void CountShouldExecuteScalar()
+        {
+            var client = Substitute.For<IGraphClient>();
+            client
+                .ExecuteScalarGremlin("g.v(123).count()", Arg.Any<NameValueCollection>())
+                .Returns("456");
+            var node = new NodeReference(123, client);
+            var result = node.Count();
+            Assert.AreEqual(456, result);
         }
 
         public class Foo { }
