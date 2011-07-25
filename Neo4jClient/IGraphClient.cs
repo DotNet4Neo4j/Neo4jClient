f89b8ac1e54b07d757b0ac3fd6050933c7c042bb
Tatham Oddie <tatham@oddie.com.au>
2011-07-26 09:44:23 +1000

    Reformatted code.

--- Neo4jClient/IGraphClient.cs
+++ Neo4jClient/IGraphClient.cs
@@ -6,20 +6,23 @@ namespace Neo4jClient
     public interface IGraphClient
     {
         RootNode RootNode { get; }
-        NodeReference<TNode> Create<TNode>(TNode node, params IRelationshipAllowingParticipantNode<TNode>[] relationships) where TNode : class;
 
-        void CreateRelationship<TSourceNode, TRelationship>(
-            NodeReference<TSourceNode> sourceNodeReference,
-            TRelationship relationship)
-            where TRelationship :
-                Relationship,
-                IRelationshipAllowingSourceNode<TSourceNode>;
+        NodeReference<TNode> Create<TNode>(TNode node, params IRelationshipAllowingParticipantNode<TNode>[] relationships)
+            where TNode : class;
 
         Node<TNode> Get<TNode>(NodeReference reference);
+
         Node<TNode> Get<TNode>(NodeReference<TNode> reference);
+
         void Update<TNode>(NodeReference<TNode> nodeReference, Action<TNode> updateCallback);
+
         void Delete(NodeReference reference, DeleteMode mode);
+
+        void CreateRelationship<TSourceNode, TRelationship>(NodeReference<TSourceNode> sourceNodeReference, TRelationship relationship)
+            where TRelationship : Relationship, IRelationshipAllowingSourceNode<TSourceNode>;
+
         string ExecuteScalarGremlin(string query);
+
         IEnumerable<Node<TNode>> ExecuteGetAllNodesGremlin<TNode>(string query);
     }
 }
