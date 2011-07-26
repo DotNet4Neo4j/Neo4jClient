c0e86ecdcf2757e8302bb727494d00180288818b
Tatham Oddie <tatham@oddie.com.au>
2011-07-26 15:32:44 +1000

    Added support for Gremlin's back method.

---
+++ Neo4jClient/Gremlin/Back.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Neo4jClient.Gremlin
+{
+    public static class Back
+    {
+        public static IGremlinNodeQuery<TNode> BackV<TNode>(this IGremlinQuery query, int pipeCount)
+        {
+            var queryText = String.Format("{0}.back({1})", query.QueryText, pipeCount);
+            return new GremlinNodeEnumerable<TNode>(query.Client, queryText);
+        }
+
+        public static IGremlinRelationshipQuery BackE(this IGremlinQuery query, int pipeCount)
+        {
+            var queryText = String.Format("{0}.back({1})", query.QueryText, pipeCount);
+            return new GremlinRelationshipEnumerable(query.Client, queryText);
+        }
+    }
+}
---
+++ Test/Gremlin/BackTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using Neo4jClient.Gremlin;
+
+namespace Neo4jClient.Test.Gremlin
+{
+    [TestFixture]
+    public class BackTests
+    {
+        [Test]
+        public void BackVWithCountShouldAppendStep()
+        {
+            var node = new NodeReference(123);
+            var queryText = node.BackV<object>(2).QueryText;
+            Assert.AreEqual("g.v(123).back(2)", queryText);
+        }
+
+        [Test]
+        public void BackVWithCountShouldReturnTypedNodeEnumerable()
+        {
+            var node = new NodeReference(123);
+            var query = node.BackV<object>(2);
+            Assert.IsInstanceOf<GremlinNodeEnumerable<object>>(query);
+        }
+
+        [Test]
+        public void BackEWithCountShouldAppendStep()
+        {
+            var node = new NodeReference(123);
+            var queryText = node.BackE(2).QueryText;
+            Assert.AreEqual("g.v(123).back(2)", queryText);
+        }
+
+        [Test]
+        public void BackEWithCountShouldReturnRelationshipEnumerable()
+        {
+            var node = new NodeReference(123);
+            var query = node.BackE(2);
+            Assert.IsInstanceOf<GremlinRelationshipEnumerable>(query);
+        }
+    }
+}
