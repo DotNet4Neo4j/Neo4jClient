5a7ff6c3ec221c0dc53d4926447575e7e13ffbf0
Romiko Derbynew <romiko.derbynew@readify.net>
2011-07-27 17:32:40 +1000

    Added expresionType to filter.

--- Neo4jClient/Gremlin/Filter.cs
+++ Neo4jClient/Gremlin/Filter.cs
@@ -1,8 +1,11 @@
-﻿namespace Neo4jClient.Gremlin
+﻿using System.Linq.Expressions;
+
+namespace Neo4jClient.Gremlin
 {
     internal struct Filter
     {
         public string PropertyName { get; set; }
         public object Value { get; set; }
+        public ExpressionType ExpressionType { get; set; }
     }
 }
