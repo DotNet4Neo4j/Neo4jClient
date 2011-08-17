ecaf2bcab02f5d90859a895aab4d38cb74160df5
Romiko Derbynew <romiko.derbynew@readify.net>
2011-08-18 08:23:15 +1000

    Added unit test for ReIndex.

---
+++ Test/GraphClientTests/ReIndexTests.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
+using NUnit.Framework;
+using RestSharp;
+
+namespace Neo4jClient.Test.GraphClientTests
+{
+    [TestFixture]
+    public class ReIndexTests
+    {
+        const string RootResponse = @"{
+                          'node' : 'http://foo/db/data/node',
+                          'node_index' : 'http://foo/db/data/index/node',
+                          'relationship_index' : 'http://foo/db/data/index/relationship',
+                          'reference_node' : 'http://foo/db/data/node/0',
+                          'extensions_info' : 'http://foo/db/data/ext',
+                          'extensions' : {
+                            'GremlinPlugin' : {
+                              'execute_script' : 'http://foo/db/data/ext/GremlinPlugin/graphdb/execute_script'
+                            }
+                          }
+                        }";
+
+        [Test]
+        public void ShoudlReturnHttpResponse201WhenCreatingAnIndexOfTypeFullText()
+        {
+            //Arrange
+            IDictionary<string, object> indexkeyValues = new Dictionary<string, object>();
+            indexkeyValues.Add("FooKey", "the_value%20with%20space");
+            var indexEntries = new List<IndexEntry>
+                {
+                   new IndexEntry
+                       {
+                           Name = "my_nodes",
+                           KeyValues = indexkeyValues,
+                       }
+                };
+
+            var restRequest = new RestRequest
+            {
+                Resource = "/index/node/my_nodes/FooKey/the_value%20with%20space",
+                Method = Method.POST,
+                RequestFormat = DataFormat.Json
+            };
+            restRequest.AddBody(restRequest.JsonSerializer.Serialize("http://foo/db/data/node/123"));
+
+            var httpFactory = MockHttpFactory.Generate("http://foo/db/data", new Dictionary<RestRequest, HttpResponse>
+            {
+                {
+                    new RestRequest { Resource = "/", Method = Method.GET },
+                    new HttpResponse
+                    {
+                        StatusCode = HttpStatusCode.OK,
+                        ContentType = "application/json",
+                        Content = RootResponse.Replace('\'', '"')
+                    }
+                },
+                {
+                    restRequest,
+                    new HttpResponse {
+                        StatusCode = HttpStatusCode.OK,
+                        ContentType = "application/json",
+                        Content = "Location: http://foo/db/data/index/node/my_nodes/FooKey/the_value%20with%20space/123"
+                    }
+                }
+            });
+            var graphClient = new GraphClient(new Uri("http://foo/db/data"), httpFactory);
+            graphClient.Connect();
+
+            //Act
+            var nodeReference = new NodeReference<TestNode>(123);
+            graphClient.ReIndex(nodeReference, indexEntries);
+
+            // Assert
+            Assert.Pass("Method executed successfully.");
+        }
+    }
+
+    public class TestNode
+    {
+        public string FooKey { get; set; }
+    }
+
+}
