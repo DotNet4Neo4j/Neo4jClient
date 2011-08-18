19243e89837d4e6a5fe140cc76ee757393695650
Romiko Derbynew <romiko.derbynew@readify.net>
2011-08-18 14:28:59 +1000

    Added IndexMetaData class.

---
+++ Neo4jClient/IndexMetaData.cs
@@ -0,0 +1,15 @@
+namespace Neo4jClient
+{
+    public class IndexMetaData
+    {
+        public bool ToLowerCase { get; set; }
+
+        public string Template { get; set; }
+
+        public string Blueprints { get; set; }
+
+        public string Provider { get; set; }
+
+        public string Type { get; set; }
+    }
+}
