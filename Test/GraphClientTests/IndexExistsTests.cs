9b88e71d0c93f7b5683d61c0eb3f050cda12a6ee
Romiko Derbynew <romiko.derbynew@readify.net>
2011-08-18 15:02:11 +1000

    Added IndexExistsTests.

---
+++ Test/GraphClientTests/IndexExistsTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using NUnit.Framework;
+using Neo4jClient.Serializer;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace Neo4jClient.Test.GraphClientTests
+{
+    [TestFixture]
+    public class IndexExistsTests
+    {
+        const string RootResponse = @"{
+                          'node' : 'http://foo/db/data/node',
+                          'node_index' : 'http://foo/db/data/index/node',
+                          'relationship_index' : 'http://foo/db/data/index/relationship',
+                          'reference_node' : 'http://foo/db/data/node/0',
+                          'extensions_info' : 'http://foo/db/data/ext',
+                          'extensions' : {
+                            'GremlinPlugin' : {
+                              'execute_script' : 'http://foo/db/data/ext/GremlinPlugin/graphdb/execute_script'
+                            }
+                          }
+                        }";
+
+        [Test]
+        public void ShoudlReturnHttResponse200()
+        {
+            //Arrange
+            var restRequest = new RestRequest("/index/node/MyIndex", Method.GET)
+            {
+                RequestFormat = DataFormat.Json,
+                JsonSerializer = new CustomJsonSerializer { NullHandling = NullValueHandling.Ignore }
+            };
+
+            var httpFactory = MockHttpFactory.Generate("http://foo/db/data", new Dictionary<RestRequest, HttpResponse>
+            {
+                {
+                    new RestRequest { Resource = "/", Method = Method.GET },
+                    new HttpResponse
+                    {
+                        StatusCode = HttpStatusCode.OK,
+                        ContentType = "application/json",
+                        Content = RootResponse.Replace('\'', '"')
+                    }
+                },
+                {
+                    restRequest,
+                    new HttpResponse {
+                        StatusCode = HttpStatusCode.OK,
+                        ContentType = "application/json",
+                        Content = @"[ ]"
+                    }
+                }
+            });
+            var graphClient = new GraphClient(new Uri("http://foo/db/data"), httpFactory);
+            graphClient.Connect();
+
+            //Act
+            graphClient.IndexExists("MyIndex",IndexFor.Node);
+
+            // Assert
+            Assert.Pass("Method executed successfully.");
+        }
+    }
+}
