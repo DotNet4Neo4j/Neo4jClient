fbd863dfde44ef0ab0157962040aef4960516398
Tatham Oddie <tatham@oddie.com.au>
2011-09-08 11:30:01 +1000

    Deleted test that covers DateTime values.

--- Test/GraphClientTests/GetNodeTests.cs
+++ Test/GraphClientTests/GetNodeTests.cs
@@ -276,67 +276,12 @@ namespace Neo4jClient.Test.GraphClientTests
             Assert.AreEqual("2011-06-30 08:15:46Z", node.Data.DateOffSet.Value.ToString("u"));
         }
 
-        [Test]
-        public void ShouldReturnNodeDataAndDeserialzedJsonDatesForDateTimeType()
-        {
-            var httpFactory = MockHttpFactory.Generate("http://foo/db/data", new Dictionary<RestRequest, HttpResponse>
-            {
-                {
-                    new RestRequest { Resource = "/", Method = Method.GET },
-                    new HttpResponse
-                    {
-                        StatusCode = HttpStatusCode.OK,
-                        ContentType = "application/json",
-                        Content = @"{
-                          'node' : 'http://foo/db/data/node',
-                          'node_index' : 'http://foo/db/data/index/node',
-                          'relationship_index' : 'http://foo/db/data/index/relationship',
-                          'reference_node' : 'http://foo/db/data/node/0',
-                          'extensions_info' : 'http://foo/db/data/ext',
-                          'extensions'' : {
-                          }
-                        }".Replace('\'', '"')
-                    }
-                },
-                {
-                    new RestRequest { Resource = "/node/456", Method = Method.GET},
-                    new HttpResponse {
-                        StatusCode = HttpStatusCode.OK,
-                        ContentType = "application/json",
-                        Content = @"{ 'self': 'http://foo/db/data/node/456',
-                          'data': { 'Foo': 'foo',
-                                    'Bar': 'bar',
-                                    'Baz': 'baz',
-                                    'Date': '/Date(1309421746929+0000)/'
-                          },
-                          'create_relationship': 'http://foo/db/data/node/456/relationships',
-                          'all_relationships': 'http://foo/db/data/node/456/relationships/all',
-                          'all_typed relationships': 'http://foo/db/data/node/456/relationships/all/{-list|&|types}',
-                          'incoming_relationships': 'http://foo/db/data/node/456/relationships/in',
-                          'incoming_typed relationships': 'http://foo/db/data/node/456/relationships/in/{-list|&|types}',
-                          'outgoing_relationships': 'http://foo/db/data/node/456/relationships/out',
-                          'outgoing_typed relationships': 'http://foo/db/data/node/456/relationships/out/{-list|&|types}',
-                          'properties': 'http://foo/db/data/node/456/properties',
-                          'property': 'http://foo/db/data/node/456/property/{key}',
-                          'traverse': 'http://foo/db/data/node/456/traverse/{returnType}'
-                        }".Replace('\'', '"')
-                    }
-                }
-            });
-
-            var graphClient = new GraphClient(new Uri("http://foo/db/data"), httpFactory);
-            graphClient.Connect();
-            var node = graphClient.Get<TestNode>(456);
-            Assert.AreEqual("2011-06-30 08:15:46Z", node.Data.Date.ToString("u"));
-        }
-
         public class TestNode
         {
             public string Foo { get; set; }
             public string Bar { get; set; }
             public string Baz { get; set; }
             public DateTimeOffset? DateOffSet { get; set; }
-            public DateTime Date { get; set; }
         }
 
         public class TestNodeWithEnum
