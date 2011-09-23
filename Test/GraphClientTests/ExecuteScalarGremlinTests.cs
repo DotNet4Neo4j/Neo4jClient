a4027829c30d302b2f1d9128edf7936b737fb312
Tatham Oddie <tatham@oddie.com.au>
2011-09-23 11:04:49 +1000

    Purged old test.

--- Test/GraphClientTests/ExecuteScalarGremlinTests.cs
+++ Test/GraphClientTests/ExecuteScalarGremlinTests.cs
@@ -67,54 +67,5 @@ namespace Neo4jClient.Test.GraphClientTests
             //Assert
             Assert.AreEqual(1, int.Parse(node));
         }
-
-        [Test]
-        public void ShouldReturnReplaceQueryParameters()
-        {
-            //Arrange
-            var httpFactory = MockHttpFactory.Generate("http://foo/db/data", new Dictionary<RestRequest, HttpResponse>
-            {
-                {
-                    new RestRequest { Resource = "/", Method = Method.GET },
-                    new HttpResponse
-                    {
-                        StatusCode = HttpStatusCode.OK,
-                        ContentType = "application/json",
-                        Content = @"{
-                          'node' : 'http://foo/db/data/node',
-                          'node_index' : 'http://foo/db/data/index/node',
-                          'relationship_index' : 'http://foo/db/data/index/relationship',
-                          'reference_node' : 'http://foo/db/data/node/0',
-                          'extensions_info' : 'http://foo/db/data/ext',
-                          'extensions' : {
-                            'GremlinPlugin' : {
-                              'execute_script' : 'http://foo/db/data/ext/GremlinPlugin/graphdb/execute_script'
-                            }
-                          }
-                        }".Replace('\'', '"')
-                    }
-                },
-                {
-                    new RestRequest {
-                        Resource = "/ext/GremlinPlugin/graphdb/execute_script",
-                        Method = Method.POST,
-                        RequestFormat = DataFormat.Json
-                    }.AddParameter("script", "foo baz query", ParameterType.GetOrPost),
-                    new HttpResponse {
-                        StatusCode = HttpStatusCode.OK,
-                        ContentType = "application/json",
-                        Content = @"1"
-                    }
-                }
-            });
-            var graphClient = new GraphClient(new Uri("http://foo/db/data"), httpFactory);
-            graphClient.Connect();
-
-            //Act
-            graphClient.ExecuteScalarGremlin("foo <<bar>> query");
-
-            //Assert
-            Assert.Inconclusive("Not actually asserting that the correct query was sent");
-        }
     }
 }
