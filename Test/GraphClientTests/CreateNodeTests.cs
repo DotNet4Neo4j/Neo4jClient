df0bb462abcc3c5427085df416e6bd1d0c619512
Tatham Oddie <tatham@oddie.com.au>
2011-09-26 12:18:52 +1000

    Updated test to match new batching behaviour.

--- Test/GraphClientTests/CreateNodeTests.cs
+++ Test/GraphClientTests/CreateNodeTests.cs
@@ -235,62 +235,65 @@ namespace Neo4jClient.Test.GraphClientTests
         }
 
         [Test]
-        [Ignore]
         public void ShouldCreateIncomingRelationship()
         {
-            var testNode2 = new TestNode2 { Foo = "foo", Bar = "bar" };
-            var testPayload = new TestPayload { Foo = "123", Bar = "456", Baz = "789" };
+            var testNode = new TestNode2 { Foo = "foo", Bar = "bar" };
+            var testRelationshipPayload = new TestPayload { Foo = "123", Bar = "456", Baz = "789" };
+            var batch = new List<BatchStep>();
+            batch.Add(Method.POST, "/node", testNode);
+            batch.Add(Method.POST, "/node/789/relationships",
+                new RelationshipTemplate { To = "{0}", Data = testRelationshipPayload, Type = "TEST_RELATIONSHIP" });
 
             var httpFactory = MockHttpFactory.Generate("http://foo/db/data", new Dictionary<RestRequest, HttpResponse>
             {
                 {
                     new RestRequest { Resource = "/", Method = Method.GET },
-                    new HttpResponse
-                    {
+                    new HttpResponse { StatusCode = HttpStatusCode.OK, ContentType = "application/json", Content = rootResponse }
+                },
+                {
+                    new RestRequest {
+                        Resource = "/batch",
+                        Method = Method.POST,
+                        RequestFormat = DataFormat.Json
+                    }.AddBody(batch),
+                    new HttpResponse {
                         StatusCode = HttpStatusCode.OK,
                         ContentType = "application/json",
-                        Content = @"{
-                          'batch' : 'http://foo/db/data/batch',
-                          'node' : 'http://foo/db/data/node',
-                          'node_index' : 'http://foo/db/data/index/node',
-                          'relationship_index' : 'http://foo/db/data/index/relationship',
-                          'reference_node' : 'http://foo/db/data/node/0',
-                          'extensions_info' : 'http://foo/db/data/ext',
-                          'extensions'' : {
-                          }
-                        }".Replace('\'', '"')
-                    }
-                },
-                {
-                    new RestRequest {
-                        Resource = "/node",
-                        Method = Method.POST,
-                        RequestFormat = DataFormat.Json
-                    }.AddBody(testNode2),
-                    new HttpResponse {
-                        StatusCode = HttpStatusCode.Created,
-                        Headers = {
-                            new HttpHeader { Name = "Location", Value ="http://foo/db/data/node/456" }
-                        }
-                    }
-                },
-                {
-                    new RestRequest {
-                        Resource = "/node/789/relationships",
-                        Method = Method.POST,
-                        RequestFormat = DataFormat.Json
-                    }
-                    .AddBody(new RelationshipTemplate
-                    {
-                        To = "http://foo/db/data/node/456",
-                        Data = testPayload,
-                        Type = "TEST_RELATIONSHIP"
-                    }),
-                    new HttpResponse {
-                        StatusCode = HttpStatusCode.Created,
-                        Headers = {
-                            new HttpHeader { Name = "Location", Value ="http://foo/db/data/relationship/123" }
-                        }
+                        Content = @"[{'id':0,'location':'http://foo/db/data/node/760','body':{
+                          'outgoing_relationships' : 'http://foo/db/data/node/760/relationships/out',
+                          'data' : {
+                            'Foo' : 'foo',
+                            'Bar' : 'bar',
+                            'Baz' : 'baz'
+                          },
+                          'traverse' : 'http://foo/db/data/node/760/traverse/{returnType}',
+                          'all_typed_relationships' : 'http://foo/db/data/node/760/relationships/all/{-list|&|types}',
+                          'self' : 'http://foo/db/data/node/760',
+                          'property' : 'http://foo/db/data/node/760/properties/{key}',
+                          'outgoing_typed_relationships' : 'http://foo/db/data/node/760/relationships/out/{-list|&|types}',
+                          'properties' : 'http://foo/db/data/node/760/properties',
+                          'incoming_relationships' : 'http://foo/db/data/node/760/relationships/in',
+                          'extensions' : {
+                          },
+                          'create_relationship' : 'http://foo/db/data/node/760/relationships',
+                          'paged_traverse' : 'http://foo/db/data/node/760/paged/traverse/{returnType}{?pageSize,leaseTime}',
+                          'all_relationships' : 'http://foo/db/data/node/760/relationships/all',
+                          'incoming_typed_relationships' : 'http://foo/db/data/node/760/relationships/in/{-list|&|types}'
+                        },'from':'/node'},{'id':1,'location':'http://foo/db/data/relationship/756','body':{
+                          'start' : 'http://foo/db/data/node/760',
+                          'data' : {
+                            'Foo' : 123,
+                            'Bar' : 456,
+                            'Baz' : 789
+                          },
+                          'property' : 'http://foo/db/data/relationship/756/properties/{key}',
+                          'self' : 'http://foo/db/data/relationship/756',
+                          'properties' : 'http://foo/db/data/relationship/756/properties',
+                          'type' : 'TEST_RELATIONSHIP',
+                          'extensions' : {
+                          },
+                          'end' : 'http://foo/db/data/node/789'
+                        },'from':'http://foo/db/data/node/761/relationships'}]".Replace('\'', '\"')
                     }
                 }
             });
@@ -299,8 +302,8 @@ namespace Neo4jClient.Test.GraphClientTests
             graphClient.Connect();
 
             graphClient.Create(
-                testNode2,
-                new TestRelationship(789, testPayload));
+                testNode,
+                new TestRelationship(789, testRelationshipPayload));
 
             Assert.Inconclusive("Not actually asserting that the relationship was created");
         }
