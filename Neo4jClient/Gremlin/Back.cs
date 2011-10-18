e46671e6c8d660febff666a9b64a87abcc62b34c
Tatham Oddie <tatham@oddie.com.au>
2011-10-19 08:19:38 +1100

    Added label support to BackV and BackE.

--- Neo4jClient/Gremlin/Back.cs
+++ Neo4jClient/Gremlin/Back.cs
@@ -8,9 +8,21 @@
             return new GremlinNodeEnumerable<TNode>(newQuery);
         }
 
+        public static IGremlinNodeQuery<TNode> BackV<TNode>(this IGremlinQuery query, string label)
+        {
+            var newQuery = query.AddBlock(".back({0})", label);
+            return new GremlinNodeEnumerable<TNode>(newQuery);
+        }
+
         public static IGremlinRelationshipQuery BackE(this IGremlinQuery query, int pipeCount)
         {
             var newQuery = query.AddBlock(".back({0})", pipeCount);
+            return new GremlinRelationshipEnumerable(newQuery);
+        }
+
+        public static IGremlinRelationshipQuery BackE(this IGremlinQuery query, string label)
+        {
+            var newQuery = query.AddBlock(".back({0})", label);
             return new GremlinRelationshipEnumerable(newQuery);
         }
 
@@ -18,6 +30,13 @@
             where TData : class, new()
         {
             var newQuery = query.AddBlock(".back({0})", pipeCount);
+            return new GremlinRelationshipEnumerable<TData>(newQuery);
+        }
+
+        public static IGremlinRelationshipQuery<TData> BackE<TData>(this IGremlinQuery query, string label)
+            where TData : class, new()
+        {
+            var newQuery = query.AddBlock(".back({0})", label);
             return new GremlinRelationshipEnumerable<TData>(newQuery);
         }
     }
--- Test/Gremlin/BackTests.cs
+++ Test/Gremlin/BackTests.cs
@@ -16,10 +16,27 @@ namespace Neo4jClient.Test.Gremlin
         }
 
         [Test]
+        public void BackVWithLabelShouldAppendStep()
+        {
+            var query = new NodeReference(123).BackV<object>("foo");
+            Assert.AreEqual("g.v(p0).back(p1)", query.QueryText);
+            Assert.AreEqual(123, query.QueryParameters["p0"]);
+            Assert.AreEqual("foo", query.QueryParameters["p1"]);
+        }
+
+        [Test]
         public void BackVWithCountShouldReturnTypedNodeEnumerable()
         {
             var node = new NodeReference(123);
             var query = node.BackV<object>(2);
+            Assert.IsInstanceOf<GremlinNodeEnumerable<object>>(query);
+        }
+
+        [Test]
+        public void BackVWithLabelShouldReturnTypedNodeEnumerable()
+        {
+            var node = new NodeReference(123);
+            var query = node.BackV<object>("foo");
             Assert.IsInstanceOf<GremlinNodeEnumerable<object>>(query);
         }
 
@@ -33,10 +50,27 @@ namespace Neo4jClient.Test.Gremlin
         }
 
         [Test]
+        public void BackEWithLabelShouldAppendStep()
+        {
+            var query = new NodeReference(123).BackE("foo");
+            Assert.AreEqual("g.v(p0).back(p1)", query.QueryText);
+            Assert.AreEqual(123, query.QueryParameters["p0"]);
+            Assert.AreEqual("foo", query.QueryParameters["p1"]);
+        }
+
+        [Test]
         public void BackEWithCountShouldReturnRelationshipEnumerable()
         {
             var node = new NodeReference(123);
             var query = node.BackE(2);
+            Assert.IsInstanceOf<GremlinRelationshipEnumerable>(query);
+        }
+
+        [Test]
+        public void BackEWithLabelShouldReturnRelationshipEnumerable()
+        {
+            var node = new NodeReference(123);
+            var query = node.BackE("foo");
             Assert.IsInstanceOf<GremlinRelationshipEnumerable>(query);
         }
     }
