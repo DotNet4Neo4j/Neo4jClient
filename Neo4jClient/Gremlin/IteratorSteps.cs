c74046181af620c6c511c65bdcf6f16ac0279069
Tatham Oddie <tatham@oddie.com.au>
2011-10-24 12:08:08 +1100

    Added GremlinSkip[E/V] support.

---
+++ Neo4jClient/Gremlin/IteratorSteps.cs
@@ -0,0 +1,24 @@
+﻿namespace Neo4jClient.Gremlin
+{
+    public static class Iterator
+    {
+        public static IGremlinNodeQuery<TNode> GremlinSkipV<TNode>(this IGremlinQuery query, int count)
+        {
+            var newQuery = query.AddBlock(".drop({0})", count);
+            return new GremlinNodeEnumerable<TNode>(newQuery);
+        }
+
+        public static IGremlinRelationshipQuery GremlinSkipE(this IGremlinQuery query, int count)
+        {
+            var newQuery = query.AddBlock(".drop({0})", count);
+            return new GremlinRelationshipEnumerable(newQuery);
+        }
+
+        public static IGremlinRelationshipQuery<TData> GremlinSkipE<TData>(this IGremlinQuery query, int count)
+            where TData : class, new()
+        {
+            var newQuery = query.AddBlock(".drop({0})", count);
+            return new GremlinRelationshipEnumerable<TData>(newQuery);
+        }
+    }
+}
---
+++ Test/Gremlin/IteratorTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using Neo4jClient.Gremlin;
+
+namespace Neo4jClient.Test.Gremlin
+{
+    [TestFixture]
+    public class IteratorTests
+    {
+        [Test]
+        public void GremlinSkipVShouldAppendStep()
+        {
+            var node = new NodeReference(123);
+            var query = node.OutV<object>().GremlinSkipV<object>(5);
+            Assert.IsInstanceOf<GremlinNodeEnumerable<object>>(query);
+            Assert.AreEqual("g.v(p0).outV.drop(p1)", query.QueryText);
+            Assert.AreEqual(123, query.QueryParameters["p0"]);
+            Assert.AreEqual(5, query.QueryParameters["p1"]);
+        }
+
+        [Test]
+        public void GremlinSkipEShouldAppendStep()
+        {
+            var node = new NodeReference(123);
+            var query = node.OutV<object>().GremlinSkipE(5);
+            Assert.IsInstanceOf<GremlinRelationshipEnumerable>(query);
+            Assert.AreEqual("g.v(p0).outV.drop(p1)", query.QueryText);
+            Assert.AreEqual(123, query.QueryParameters["p0"]);
+            Assert.AreEqual(5, query.QueryParameters["p1"]);
+        }
+
+        [Test]
+        public void GremlinSkipEWithTDataShouldAppendStep()
+        {
+            var node = new NodeReference(123);
+            var query = node.OutV<object>().GremlinSkipE<object>(5);
+            Assert.IsInstanceOf<GremlinRelationshipEnumerable<object>>(query);
+            Assert.AreEqual("g.v(p0).outV.drop(p1)", query.QueryText);
+            Assert.AreEqual(123, query.QueryParameters["p0"]);
+            Assert.AreEqual(5, query.QueryParameters["p1"]);
+        }
+    }
+}
