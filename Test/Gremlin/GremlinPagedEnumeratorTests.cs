7390cdad72269df8f20a302347dcbfcbfeb87a99
Tatham Oddie <tatham@oddie.com.au>
2011-10-24 12:52:59 +1100

    Added test for enumerating over two pages of results.

--- Test/Gremlin/GremlinPagedEnumeratorTests.cs
+++ Test/Gremlin/GremlinPagedEnumeratorTests.cs
@@ -117,5 +117,31 @@ namespace Neo4jClient.Test.Gremlin
             Assert.AreEqual(100, loadedQueries[1].QueryParameters["p1"]);
             Assert.AreEqual(100, loadedQueries[1].QueryParameters["p2"]);
         }
+
+        [Test]
+        public void ShouldEnumerateOverTwoPagesOfResults()
+        {
+            var pages = new Queue<IEnumerable<int>>(new[]
+            {
+                Enumerable.Range(0, 100),
+                Enumerable.Range(100, 100)
+            });
+
+            var loadedQueries = new List<IGremlinQuery>();
+            Func<IGremlinQuery, IEnumerable<int>> loadCallback =
+                q => { loadedQueries.Add(q); return pages.Dequeue(); };
+
+            var baseQuery = new GremlinQuery(
+                null,
+                "g.v(p0).outV",
+                new Dictionary<string, object> { { "p0", 0 } });
+
+            var enumerator = new GremlinPagedEnumerator<int>(loadCallback, baseQuery);
+            for (var i = 0; i < 200; i++)
+            {
+                Assert.IsTrue(enumerator.MoveNext());
+                Assert.AreEqual(i, enumerator.Current);
+            }
+        }
     }
 }
