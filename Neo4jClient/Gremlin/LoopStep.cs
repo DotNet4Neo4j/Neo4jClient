061801938cc627119ffe167d50e2cd38985c901a
Tatham Oddie <tatham@oddie.com.au>
2011-11-03 06:42:03 +1100

    Added support for Gremlin's loop pattern.

---
+++ Neo4jClient/Gremlin/LoopStep.cs
@@ -0,0 +1,24 @@
+﻿namespace Neo4jClient.Gremlin
+{
+    public static class LoopStep
+    {
+        public static IGremlinNodeQuery<TNode> LoopV<TNode>(this IGremlinQuery query, string label, uint loopCount)
+        {
+            var newQuery = query.AddBlock(".loop({0}){{ it.loops < {1} }}", label, loopCount);
+            return new GremlinNodeEnumerable<TNode>(newQuery);
+        }
+
+        public static IGremlinRelationshipQuery LoopE(this IGremlinQuery query, string label, uint loopCount)
+        {
+            var newQuery = query.AddBlock(".loop({0}){{ it.loops < {1} }}", label, loopCount);
+            return new GremlinRelationshipEnumerable(newQuery);
+        }
+
+        public static IGremlinRelationshipQuery<TData> LoopE<TData>(this IGremlinQuery query, string label, uint loopCount)
+            where TData : class, new()
+        {
+            var newQuery = query.AddBlock(".loop({0}){{ it.loops < {1} }}", label, loopCount);
+            return new GremlinRelationshipEnumerable<TData>(newQuery);
+        }
+    }
+}
