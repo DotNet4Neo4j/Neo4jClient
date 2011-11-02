a80847c0b7c868e36dd04605c8d91ec16f6174c0
Tatham Oddie <tatham@oddie.com.au>
2011-11-03 08:16:13 +1100

    Added note about where I found the test value.

--- Test/ApiModels/RootApiResponseTests.cs
+++ Test/ApiModels/RootApiResponseTests.cs
@@ -8,7 +8,7 @@ namespace Neo4jClient.Test.ApiModels
     {
         [Test]
         [TestCase("", Result = "0.0")]
-        [TestCase("1.5-82-g7cb21bb1-dirty", Result = "1.5.82")]
+        [TestCase("1.5-82-g7cb21bb1-dirty", Result = "1.5.82", Description = "http://docs.neo4j.org/chunked/snapshot/rest-api-service-root.html")]
         public string Version(string versionString)
         {
             var response = new RootApiResponse { VersionString = versionString };
