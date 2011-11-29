9a60283260c0abed04a8beeed53e9f614f27930f
Romiko Derbynew <romiko.derbynew@readify.net>
2011-11-30 10:37:43 +1100

    Added unit test to ensure GremlinTableCapResponseToResults.

---
+++ Test/ApiModels/GremlinTableCapResponseTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Neo4jClient.ApiModels;
+using System.Linq;
+
+namespace Neo4jClient.Test.ApiModels
+{
+    [TestFixture]
+    public class GremlinTableCapResponseTests
+    {
+        [Test]
+        public void VerifyTransferTableCapResponseToResult()
+        {
+            var list = new List<List<GremlinTableCapResponse>>();
+            var dataforfoo = "DataForFoo";
+            var dataforbar = "DataForBar";
+            list.Add(new List<GremlinTableCapResponse>
+                {
+                    new GremlinTableCapResponse
+                        {
+                            Columns = new List<string>
+                                {
+                                    "Foo",
+                                    "Bar"
+                                },
+                            Data = new List<List<string>>
+                                {
+                                    new List<string>
+                                        {
+                                            dataforfoo,
+                                            dataforbar,
+                                        }
+                                }
+                        }
+                });
+            var response = GremlinTableCapResponse.TransferResponseToResult<SimpleClass>(list).ToArray();
+
+            Assert.IsTrue(response.Any(r => r.Foo == dataforfoo));
+            Assert.IsTrue(response.Any(r => r.Bar == dataforbar));
+        }
+
+    public class  SimpleClass
+        {
+            public string Foo { get; set; }
+            public string Bar { get; set; }
+        }
+    }
+}
