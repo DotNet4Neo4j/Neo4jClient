af449474ac111c2f1752479f7151d78ab6bd4163
Romiko Derbynew <romiko.derbynew@readify.net>
2011-12-02 09:45:18 +1100

    Added more realistic tests when emitting a variable from a gremlin query.

--- Test/Gremlin/EmitVariableStepTests.cs
+++ Test/Gremlin/EmitVariableStepTests.cs
@@ -9,31 +9,38 @@ namespace Neo4jClient.Test.Gremlin
         [Test]
         public void EmitVariableShouldAppendStepToNodeQuery()
         {
-            var query = new NodeReference(123).OutV<object>().EmitVariableV<object>("x");
+            var query = new NodeReference(123).OutV<object>().AggregateV<object>("x").OutV<object>().EmitVariableV<object>("x");
             Assert.IsInstanceOf<IGremlinNodeQuery<object>>(query);
-            Assert.AreEqual("g.v(p0).outV.p1", query.QueryText);
+            Assert.AreEqual("p2 = [];g.v(p0).outV.aggregate(p1).outV.p3", query.QueryText);
             Assert.AreEqual(123, query.QueryParameters["p0"]);
             Assert.AreEqual("x", query.QueryParameters["p1"]);
+            Assert.AreEqual("x", query.QueryParameters["p2"]);
         }
 
         [Test]
         public void EmitVariableShouldAppendStepToRelationshipQuery()
         {
-            var query = new NodeReference(123).OutE().EmitVariableE("x");
+            var query = new NodeReference(123).OutE().AggregateE("x").OutE("mylabel").EmitVariableE("x");
             Assert.IsInstanceOf<IGremlinRelationshipQuery>(query);
-            Assert.AreEqual("g.v(p0).outE.p1", query.QueryText);
+            Assert.AreEqual("p2 = [];g.v(p0).outE.aggregate(p1).outE[[label:p3]].p4", query.QueryText);
             Assert.AreEqual(123, query.QueryParameters["p0"]);
             Assert.AreEqual("x", query.QueryParameters["p1"]);
+            Assert.AreEqual("x", query.QueryParameters["p2"]);
+            Assert.AreEqual("mylabel", query.QueryParameters["p3"]);
+            Assert.AreEqual("x", query.QueryParameters["p4"]);
         }
 
         [Test]
         public void EmitVariableShouldAppendStepToTypedRelationshipQuery()
         {
-            var query = new NodeReference(123).OutE<object>().EmitVariableE<object>("x");
-            Assert.IsInstanceOf<IGremlinRelationshipQuery<object>>(query);
-            Assert.AreEqual("g.v(p0).outE.p1", query.QueryText);
+            var query = new NodeReference(123).OutE<object>().AggregateE("x").OutE<object>("mylabel").EmitVariableE("x");
+            Assert.IsInstanceOf<IGremlinRelationshipQuery>(query);
+            Assert.AreEqual("p2 = [];g.v(p0).outE.aggregate(p1).outE[[label:p3]].p4", query.QueryText);
             Assert.AreEqual(123, query.QueryParameters["p0"]);
             Assert.AreEqual("x", query.QueryParameters["p1"]);
+            Assert.AreEqual("x", query.QueryParameters["p2"]);
+            Assert.AreEqual("mylabel", query.QueryParameters["p3"]);
+            Assert.AreEqual("x", query.QueryParameters["p4"]);
         }
     }
 }
