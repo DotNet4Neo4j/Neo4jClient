9179b89b57f9a8d9b34f9c109ec17bfa8bb256f6
Romiko Derbynew <romiko.derbynew@readify.net>
2011-12-06 21:37:55 +1100

    Remove white space.

--- Test/Gremlin/IfThenElseTests.cs
+++ Test/Gremlin/IfThenElseTests.cs
@@ -10,7 +10,7 @@ namespace Neo4jClient.Test.Gremlin
         public void IfThenElseVShouldAppendSteps()
         {
             var query = new NodeReference(123).IfThenElse(
-                new GremlinIterator().OutV<object>().GremlinHasNext(), 
+                new GremlinIterator().OutV<object>().GremlinHasNext(),
                 null,
                 null);
             Assert.AreEqual("g.v(p0).ifThenElse{it.outV.hasNext()}{}{}", query.QueryText);
