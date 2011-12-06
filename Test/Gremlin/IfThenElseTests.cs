7b58df1302f37b8016b3d29f675c3ab30c00f1f7
Romiko Derbynew <romiko.derbynew@readify.net>
2011-12-06 21:40:15 +1100

    Added unit test IfThenElseVShouldAppendStepsWithThenQueryAndElseQuery

--- Test/Gremlin/IfThenElseTests.cs
+++ Test/Gremlin/IfThenElseTests.cs
@@ -15,5 +15,15 @@ namespace Neo4jClient.Test.Gremlin
                 null);
             Assert.AreEqual("g.v(p0).ifThenElse{it.outV.hasNext()}{}{}", query.QueryText);
         }
+
+        [Test]
+        public void IfThenElseVShouldAppendStepsWithThenQueryAndElseQuery()
+        {
+            var query = new NodeReference(123).IfThenElse(
+                new GremlinIterator().OutV<object>().GremlinHasNext(),
+                new IdentityPipe().OutV<object>(),
+                new IdentityPipe().InV<object>());
+            Assert.AreEqual("g.v(p0).ifThenElse{it.outV.hasNext()}{_().outV}{_().inV}", query.QueryText);
+        }
     }
 }
