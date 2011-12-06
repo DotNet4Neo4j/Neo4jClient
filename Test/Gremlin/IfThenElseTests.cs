5344d861643e1596ab139b8e6384ece9c8ed3d39
Romiko Derbynew <romiko.derbynew@readify.net>
2011-12-06 21:37:16 +1100

    Added basic unit test for ifthenelse.

---
+++ Test/Gremlin/IfThenElseTests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using Neo4jClient.Gremlin;
+
+namespace Neo4jClient.Test.Gremlin
+{
+    [TestFixture]
+    public class IfThenElseTests
+    {
+        [Test]
+        public void IfThenElseVShouldAppendSteps()
+        {
+            var query = new NodeReference(123).IfThenElse(
+                new GremlinIterator().OutV<object>().GremlinHasNext(), 
+                null,
+                null);
+            Assert.AreEqual("g.v(p0).ifThenElse{it.outV.hasNext()}{}{}", query.QueryText);
+        }
+    }
+}
