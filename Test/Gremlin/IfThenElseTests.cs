fe48bc3ebe4b9d97223c06da99b101057979dc37
Romiko Derbynew <romiko.derbynew@readify.net>
2011-12-06 21:47:55 +1100

    Added IfThenElseVShouldAppendStepsWithThenQueryAndElseQueryWithParameters

--- Test/Gremlin/IfThenElseTests.cs
+++ Test/Gremlin/IfThenElseTests.cs
@@ -25,5 +25,28 @@ namespace Neo4jClient.Test.Gremlin
                 new IdentityPipe().InV<object>());
             Assert.AreEqual("g.v(p0).ifThenElse{it.outV.hasNext()}{_().outV}{_().inV}", query.QueryText);
         }
+
+        [Test]
+        public void IfThenElseVShouldAppendStepsWithThenQueryAndElseQueryWithParameters()
+        {
+            var query = new NodeReference(123).IfThenElse(
+                new GremlinIterator().OutV<Test>(t => t.Flag == true).GremlinHasNext(),
+                new IdentityPipe().OutV<Test>(t => t.Name == "foo"),
+                new IdentityPipe().InV<Test>(t => t.Name == "bar"));
+            Assert.AreEqual("g.v(p0).ifThenElse{it.outV.filter{ it[p1] == p2 }.hasNext()}{_().outV.filter{ it[p3].equalsIgnoreCase(p4) }}{_().inV.filter{ it[p5].equalsIgnoreCase(p6) }}", query.QueryText);
+            Assert.AreEqual(123, query.QueryParameters["p0"]);
+            Assert.AreEqual("Flag", query.QueryParameters["p1"]);
+            Assert.AreEqual(true, query.QueryParameters["p2"]);
+            Assert.AreEqual("Name", query.QueryParameters["p3"]);
+            Assert.AreEqual("foo", query.QueryParameters["p4"]);
+            Assert.AreEqual("Name", query.QueryParameters["p5"]);
+            Assert.AreEqual("bar", query.QueryParameters["p6"]);
+        }
+
+        public class Test
+        {
+            public bool Flag { get; set; }
+            public string Name { get; set; }
+        }
     }
 }
