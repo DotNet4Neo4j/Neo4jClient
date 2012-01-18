519b2611e97ab5eca2a0f85515f7813b60d07c33
Romiko Derbynew <romiko.derbynew@readify.net>
2012-01-18 21:32:00 +1100

    Added tests for Cypher Start step.

---
+++ Test/Cypher/StartStepTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+using Neo4jClient.Cypher;
+
+namespace Neo4jClient.Test.Cypher
+{
+    [TestFixture]
+    public class StartStepTests
+    {
+        [Test]
+        public void StartShouldCreateStepToNodeQuery()
+        {
+            var client = new GraphClient(new Uri("http://foo/db/data"));
+            var query = client.Cypher.Start<object>("myNode",new[] {200});
+            Assert.IsInstanceOf<ICypherNodeQuery<object>>(query);
+            Assert.AreEqual("start myNode=node(p0)", query.QueryText);
+            Assert.AreEqual(200, query.QueryParameters["p0"]);
+        }
+
+        [Test]
+        public void StartShouldCreateStepToNodesQuery()
+        {
+            var client = new GraphClient(new Uri("http://foo/db/data"));
+            var query = client.Cypher.Start<object>("myNode", new[] { 200, 201 });
+            Assert.IsInstanceOf<ICypherNodeQuery<object>>(query);
+            Assert.AreEqual("start myNode=node(p0,p1)", query.QueryText);
+            Assert.AreEqual(200, query.QueryParameters["p0"]);
+            Assert.AreEqual(201, query.QueryParameters["p1"]);
+        }
+    }
+}
