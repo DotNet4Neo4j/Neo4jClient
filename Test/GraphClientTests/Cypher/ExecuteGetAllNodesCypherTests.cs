38716836ba546e7668a9a69b20624f7fdbeca360
Romiko Derbynew <romiko.derbynew@readify.net>
2012-01-18 17:54:58 +1100

    Added tests for ExecuteGetAllRelationshipsCypherTests

--- Test/GraphClientTests/Cypher/ExecuteGetAllNodesCypherTests.cs
+++ Test/GraphClientTests/Cypher/ExecuteGetAllNodesCypherTests.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using NUnit.Framework;
-using Neo4jClient.ApiModels;
 using Neo4jClient.ApiModels.Cypher;
 using RestSharp;
 
