b3eb99d36e964de495ace243248a16b0a8e3e9b8
Romiko Derbynew <romiko.derbynew@readify.net>
2012-01-19 08:15:36 +1100

    Added support for Start Step to use NodeReferences and RelationshipReferences.

--- Neo4jClient/Cypher/StartStep.cs
+++ Neo4jClient/Cypher/StartStep.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Neo4jClient.Cypher
 {
@@ -23,6 +24,22 @@ namespace Neo4jClient.Cypher
         /// <summary>
         /// Creates a new Cypher Start Query, all previous steps ignored
         /// </summary>
+        public static ICypherNodeQuery<TNode> StartV<TNode>(this ICypherQuery query, string declaration, NodeReference<TNode>[] nodeReferences)
+        {
+            var startText = string.Format("start {0}=", declaration);
+            var parameters = new string[nodeReferences.Length];
+            for (var i = 0; i < nodeReferences.Length; i++)
+            {
+                parameters[i] = string.Format("p{0}", i);
+            }
+
+            var newQuery = query.BuildStartById(string.Format("{0}node({1})", startText, String.Join(",", parameters)), nodeReferences.Select(n => n.Id).ToArray());
+            return new CypherNodeEnumerable<TNode>(newQuery);
+        }
+
+        /// <summary>
+        /// Creates a new Cypher Start Query, all previous steps ignored
+        /// </summary>
         public static ICypherRelationshipQuery<TData> StartE<TData>(this ICypherQuery query, string declaration, int[] relatoinshipIds)
             where TData : class, new()
         {
@@ -34,6 +51,23 @@ namespace Neo4jClient.Cypher
             }
 
             var newQuery = query.BuildStartById(string.Format("{0}relationship({1})", startText, String.Join(",", parameters)), relatoinshipIds);
+            return new CypherRelationshipEnumerable<TData>(newQuery);
+        }
+
+        /// <summary>
+        /// Creates a new Cypher Start Query, all previous steps ignored
+        /// </summary>
+        public static ICypherRelationshipQuery<TData> StartE<TData>(this ICypherQuery query, string declaration, RelationshipReference[] relatoinships)
+            where TData : class, new()
+        {
+            var startText = string.Format("start {0}=", declaration);
+            var parameters = new string[relatoinships.Length];
+            for (var i = 0; i < relatoinships.Length; i++)
+            {
+                parameters[i] = string.Format("p{0}", i);
+            }
+
+            var newQuery = query.BuildStartById(string.Format("{0}relationship({1})", startText, String.Join(",", parameters)), relatoinships.Select(r => r.Id).ToArray());
             return new CypherRelationshipEnumerable<TData>(newQuery);
         }
     }
--- Test/Cypher/StartStepTests.cs
+++ Test/Cypher/StartStepTests.cs
@@ -29,6 +29,17 @@ namespace Neo4jClient.Test.Cypher
         }
 
         [Test]
+        public void StartShouldCreateStepToNodesQueryUsingNodeReference()
+        {
+            var client = new GraphClient(new Uri("http://foo/db/data"));
+            var query = client.Cypher.StartV<object>("myNodes", new[] { new NodeReference<object>(200), new NodeReference<object>(201) });
+            Assert.IsInstanceOf<ICypherNodeQuery<object>>(query);
+            Assert.AreEqual("start myNodes=node(p0,p1)", query.QueryText);
+            Assert.AreEqual(200, query.QueryParameters["p0"]);
+            Assert.AreEqual(201, query.QueryParameters["p1"]);
+        }
+
+        [Test]
         public void StartShouldCreateStepToRelationshipQuery()
         {
             var client = new GraphClient(new Uri("http://foo/db/data"));
@@ -43,6 +54,17 @@ namespace Neo4jClient.Test.Cypher
         {
             var client = new GraphClient(new Uri("http://foo/db/data"));
             var query = client.Cypher.StartE<object>("myRelationships", new[] { 200, 201 });
+            Assert.IsInstanceOf<ICypherRelationshipQuery<object>>(query);
+            Assert.AreEqual("start myRelationships=relationship(p0,p1)", query.QueryText);
+            Assert.AreEqual(200, query.QueryParameters["p0"]);
+            Assert.AreEqual(201, query.QueryParameters["p1"]);
+        }
+
+        [Test]
+        public void StartShouldCreateStepToRelationshipsQueryUsingRelationshipReference()
+        {
+            var client = new GraphClient(new Uri("http://foo/db/data"));
+            var query = client.Cypher.StartE<object>("myRelationships", new[] { new RelationshipReference(200), new RelationshipReference(201), });
             Assert.IsInstanceOf<ICypherRelationshipQuery<object>>(query);
             Assert.AreEqual("start myRelationships=relationship(p0,p1)", query.QueryText);
             Assert.AreEqual(200, query.QueryParameters["p0"]);
