f6e799be81e76c68aa5eb6509a3654dee6089bad
Tatham Oddie <tatham@oddie.com.au>
2012-02-06 14:38:49 +1100

    Refactor repeated signatures into a single interface

--- Neo4jClient/Cypher/ICypherFluentQueryMatched.cs
+++ Neo4jClient/Cypher/ICypherFluentQueryMatched.cs
@@ -1,15 +1,6 @@
-using System;
-using System.Linq.Expressions;
-
 namespace Neo4jClient.Cypher
 {
-    public interface ICypherFluentQueryMatched : ICypherFluentQuery
+    public interface ICypherFluentQueryMatched : ICypherFluentQuery, ICypherFluentQueryReturnable
     {
-        ICypherFluentQueryReturned Return(params string[] identities);
-        ICypherFluentQueryReturned ReturnDistinct(params string[] identities);
-        ICypherFluentQueryReturned Return<TResult>(Expression<Func<ICypherResultItem, TResult>> expression)
-            where TResult : new();
-        ICypherFluentQueryReturned ReturnDistinct<TResult>(Expression<Func<ICypherResultItem, TResult>> expression)
-            where TResult : new();
     }
 }
--- Neo4jClient/Cypher/ICypherFluentQueryStarted.cs
+++ Neo4jClient/Cypher/ICypherFluentQueryStarted.cs
@@ -1,18 +1,9 @@
-using System;
-using System.Linq.Expressions;
-
 namespace Neo4jClient.Cypher
 {
-    public interface ICypherFluentQueryStarted : ICypherFluentQuery
+    public interface ICypherFluentQueryStarted : ICypherFluentQuery, ICypherFluentQueryReturnable
     {
         ICypherFluentQueryStarted AddStartPoint(string identity, params NodeReference[] nodeReferences);
         ICypherFluentQueryStarted AddStartPoint(string identity, params RelationshipReference[] relationshipReferences);
         ICypherFluentQueryMatched Match(string matchText);
-        ICypherFluentQueryReturned Return(params string[] identities);
-        ICypherFluentQueryReturned ReturnDistinct(params string[] identities);
-        ICypherFluentQueryReturned Return<TResult>(Expression<Func<ICypherResultItem, TResult>> expression)
-            where TResult : new();
-        ICypherFluentQueryReturned ReturnDistinct<TResult>(Expression<Func<ICypherResultItem, TResult>> expression)
-            where TResult : new();
     }
 }
