5dddabc71a38030603bc57981bf7fb317da550c1
Romiko Derbynew <romiko.derbynew@readify.net>
2012-02-15 16:21:42 +1100

    Added CypherFluentQuery Where overloads.

---
+++ Neo4jClient/Cypher/CypherFluentQuery`Where.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Neo4jClient.Cypher
+{
+    public partial class CypherFluentQuery
+    {
+        internal ICypherFluentQueryWhere Where(LambdaExpression expression)
+        {
+            var newBuilder = Builder.SetWhere(expression);
+            return new CypherFluentQuery(Client, newBuilder);
+        }
+
+        public ICypherFluentQueryWhere Where<T1>(Expression<Func<T1, bool>> expression)
+        {
+            return Where((LambdaExpression)expression);
+        }
+
+        public ICypherFluentQueryWhere Where<T1, T2>(Expression<Func<T1, T2, bool>> expression)
+        {
+            return Where((LambdaExpression)expression);
+        }
+    }
+}
