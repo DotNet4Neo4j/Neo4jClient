4a28a84081d80a5ed627d98693de92901311b858
Romiko Derbynew <romiko.derbynew@readify.net>
2012-02-15 16:22:58 +1100

    Added unit test for Where Clause.

--- Test/Cypher/CypherFluentQueryTests.cs
+++ Test/Cypher/CypherFluentQueryTests.cs
@@ -257,9 +257,30 @@ namespace Neo4jClient.Test.Cypher
             Assert.AreEqual(1, query.QueryParameters["p0"]);
         }
 
+        [Test]
+        public void WhereBooleanOperations()
+        {
+            // http://docs.neo4j.org/chunked/1.6/query-where.html#where-boolean-operations
+            // START n=node(3, 1)
+            // WHERE (n.age < 30 and n.name = "Tobias") or not(n.name = "Tobias")
+            // RETURN n
+
+            var client = Substitute.For<IGraphClient>();
+            var query = new CypherFluentQuery(client)
+                .Start("n", (NodeReference)3, (NodeReference)1)
+                .Where<FooNode>(n => (n.Age < 30 && n.Name == "Tobias") || n.Name != "Tobias")
+                .Return<object>("n")
+                .Query;
+
+            Assert.AreEqual("START a=node({p0})\r\nWHERE (n.age < 30 and n.name = 'Tobias') or not(n.name = 'Tobias')\r\nRETURN n".Replace("'","\""), query.QueryText);
+            Assert.AreEqual(3, query.QueryParameters["p0"]);
+            Assert.AreEqual(1, query.QueryParameters["p1"]);
+        }
+
         public class FooNode
         {
             public int Age { get; set; }
+            public string Name { get; set; }
         }
 
         public class ReturnPropertyQueryResult
