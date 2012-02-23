377337a691e45dfcaf989e58f211c8643fa00caa
Tatham Oddie <tatham@oddie.com.au>
2012-02-23 17:34:42 +0000

    Fix casing

--- Neo4jClient/Cypher/ICypherFluentQueryReturned.cs
+++ Neo4jClient/Cypher/ICypherFluentQueryReturned.cs
@@ -5,7 +5,7 @@ namespace Neo4jClient.Cypher
     public interface ICypherFluentQueryReturned<out TResult> : ICypherFluentQuery
     {
         ICypherFluentQueryReturned<TResult> Limit(int? limit);
-        ICypherFluentQueryReturned<TResult> Skip(int? Skip);
+        ICypherFluentQueryReturned<TResult> Skip(int? skip);
         ICypherFluentQueryReturned<TResult> OrderBy(params string[] properties);
         ICypherFluentQueryReturned<TResult> OrderByDescending(params string[] properties);
         IEnumerable<TResult> Results { get; }
