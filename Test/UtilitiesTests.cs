3da99208f31c9b1fa455e3755801115e3efba883
Jamal Abreu <jabreu@barnardos.org.au>
2012-05-29 11:35:49 +1000

    Created tests for utilities tests

--- Test/UtilitiesTests.cs
+++ Test/UtilitiesTests.cs
@@ -14,15 +14,18 @@ namespace Neo4jClient.Test
         [Test]
         public void ShouldReturnDifferenceBetweenDictionaries()
         {
-            var dic1 = new Dictionary<string, string> {{"key1", "value1"}, {"key2", "value2"}, {"key3", "value3"}};
+            var dic1 = new Dictionary<string, string> {{ "key1", "value1" }, { "key2", "value2" }, { "key3", "value3" }};
+            var dic2 = new Dictionary<string, string> {{ "key1", "newValue1" }, { "key2", "value2" }, { "key3", "newValue3" }};
+            var dic3 = new Dictionary<string, string> { { "key1", "value1" }, { "key2", "value2" }, { "key3", "value3" } };
 
-            var dic2 = new Dictionary<string, string> {{"key1", "foo"}, {"key2", "value2"}, {"key3", "bar"}};
+            var differences12 = Utilities.GetDifferencesBetweenDictionaries(dic1, dic2).ToArray();
+            var differences13 = Utilities.GetDifferencesBetweenDictionaries(dic1, dic3).ToArray();
 
-            var differences = Utilities.GetDifferencesBetweenDictionaries(dic1, dic2);
+            Assert.IsTrue(differences12.Count() == 2);
+            Assert.IsTrue(differences12.Any(d=>d.FieldName == "key1" && d.OldValue == "value1" && d.NewValue == "newValue1"));
+            Assert.IsTrue(differences12.Any(d => d.FieldName == "key3" && d.OldValue == "value3" && d.NewValue == "newValue3"));
+            Assert.IsTrue(!differences13.Any());
 
-
-
-            //Assert.AreEqual(client, ((IGremlinQuery)node).Client);
         }
     }
 }
