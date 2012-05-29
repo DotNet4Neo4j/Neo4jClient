8b101df13f9e55d99fffc15ab3fc28bc09b04c06
Jamal Abreu <jabreu@barnardos.org.au>
2012-05-29 10:30:36 +1000

    Added utilityTests class

--- Neo4jClient/Utilities.cs
+++ Neo4jClient/Utilities.cs
@@ -2,10 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Neo4jClient.ApiModels;
 
 namespace Neo4jClient
 {
-    class Utilities
+    public class Utilities
     {
+        public static IEnumerable<FieldChange> GetDifferencesBetweenDictionaries(IDictionary<string, string> originalValues, IDictionary<string, string> newValues)
+        {
+            return originalValues
+                .Keys
+                .Select(k => new FieldChange
+                    {
+                        FieldName = k,
+                        OldValue = originalValues[k],
+                        NewValue = newValues[k]
+                    })
+                .Where(c => !c.OldValue.Equals(c.NewValue, StringComparison.Ordinal));
+        }
     }
 }
---
+++ Test/UtilitiesTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NSubstitute;
+using NUnit.Framework;
+using Neo4jClient.Gremlin;
+
+namespace Neo4jClient.Test
+{
+    [TestFixture]
+    public class UtilitiesTests
+    {
+        [Test]
+        public void ShouldReturnDifferenceBetweenDictionaries()
+        {
+            var dic1 = new Dictionary<string, string> {{"key1", "value1"}, {"key2", "value2"}, {"key3", "value3"}};
+
+            var dic2 = new Dictionary<string, string> {{"key1", "foo"}, {"key2", "value2"}, {"key3", "bar"}};
+
+            var differences = Utilities.GetDifferencesBetweenDictionaries(dic1, dic2);
+
+
+
+            //Assert.AreEqual(client, ((IGremlinQuery)node).Client);
+        }
+    }
+}
