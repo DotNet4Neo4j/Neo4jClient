c66ca607a75cba8c549469f50b9dfcdc59ba6507
Tatham Oddie <tatham@oddie.com.au>
2012-06-27 18:24:58 +1000

    Fix method casing

--- Test/CultureInfoSetupFixture.cs
+++ Test/CultureInfoSetupFixture.cs
@@ -8,7 +8,7 @@ namespace Neo4jClient.Test
     public class CultureInfoSetupFixture
     {
         [SetUp]
-        public void SetCultureToSomethingNonLatinToEnsureCodeUnderTestDoesntAssumeEnAU()
+        public void SetCultureToSomethingNonLatinToEnsureCodeUnderTestDoesntAssumeEnAu()
         {
             // Issue http://hg.readify.net/neo4jclient/issue/15/take-cultureinfo-into-account-for-proper
             var thread = Thread.CurrentThread;
