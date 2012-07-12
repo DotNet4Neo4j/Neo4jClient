3c88bac2c0fc2368ef37f0159b1de437d841e597
Romiko Derbynew <romiko.derbynew@readify.net>
2012-07-12 15:10:54 +1000

    Added paramterless constructor for RootNode.

--- Neo4jClient/RootNode.cs
+++ Neo4jClient/RootNode.cs
@@ -4,8 +4,8 @@ namespace Neo4jClient
 {
     public class RootNode : NodeReference<RootNode>
     {
+        public RootNode() : base(0) { }
         public RootNode(int id) : base(id) {}
-
         public RootNode(int id, IGraphClient client) : base(id, client) {}
 
         public ICypherFluentQueryStarted StartCypher(string identity)
