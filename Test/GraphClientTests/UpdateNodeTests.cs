aeac98d8ffbe062775106da8f8d1e7ebf4609293
Romiko Derbynew <romiko.derbynew@readify.net>
2012-08-28 08:16:04 +1000

    Refactor UpdateNodeTests.

--- Test/GraphClientTests/UpdateNodeTests.cs
+++ Test/GraphClientTests/UpdateNodeTests.cs
@@ -3,46 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using NUnit.Framework;
-using Neo4jClient.Serializer;
-using Newtonsoft.Json;
-using RestSharp;
 
 namespace Neo4jClient.Test.GraphClientTests
 {
     [TestFixture]
     public class UpdateNodeTests
     {
-        readonly string rootResponse = @"{
-                'batch' : 'http://foo/db/data/batch',
-                'node' : 'http://foo/db/data/node',
-                'node_index' : 'http://foo/db/data/index/node',
-                'relationship_index' : 'http://foo/db/data/index/relationship',
-                'reference_node' : 'http://foo/db/data/node/0',
-                'neo4j_version' : '1.5.M02',
-                'extensions_info' : 'http://foo/db/data/ext',
-                'extensions' : {
-                }
-            }"
-            .Replace('\'', '"');
-
-        readonly string pre15M02RootResponse = @"{
-                'batch' : 'http://foo/db/data/batch',
-                'node' : 'http://foo/db/data/node',
-                'node_index' : 'http://foo/db/data/index/node',
-                'relationship_index' : 'http://foo/db/data/index/relationship',
-                'reference_node' : 'http://foo/db/data/node/0',
-                'extensions_info' : 'http://foo/db/data/ext',
-                'extensions' : {
-                }
-            }"
-            .Replace('\'', '"');
-
         [Test]
         public void ShouldUpdateNode()
         {
             var nodeToUpdate = new TestNode { Foo = "foo", Bar = "bar", Baz = "baz" };
 
-            using (var testHarness = new RestTestHarness()
+            using (var testHarness = new RestTestHarness
                 {
                     {
                         MockRequest.Get("/node/456"),
@@ -93,23 +65,11 @@ namespace Neo4jClient.Test.GraphClientTests
         {
             var nodeToUpdate = new TestNode { Foo = "foo", Bar = "bar", Baz = "baz" };
 
-            var httpFactory = MockHttpFactory.Generate("http://foo/db/data", new Dictionary<IRestRequest, IHttpResponse>
-            {
+            using (var testHarness = new RestTestHarness
                 {
-                    new RestRequest { Resource = "", Method = Method.GET },
-                    new NeoHttpResponse
                     {
-                        StatusCode = HttpStatusCode.OK,
-                        ContentType = "application/json",
-                        TestContent = rootResponse
-                    }
-                },
-                 {
-                    new RestRequest { Resource = "/node/456", Method = Method.GET },
-                    new NeoHttpResponse {
-                        StatusCode = HttpStatusCode.OK,
-                        ContentType = "application/json",
-                        TestContent = @"{ 'self': 'http://foo/db/data/node/456',
+                        MockRequest.Get("/node/456"),
+                        MockResponse.Json(HttpStatusCode.OK, @"{ 'self': 'http://foo/db/data/node/456',
                           'data': { 'Foo': 'foo',
                                     'Bar': 'bar',
                                     'Baz': 'baz'
@@ -124,54 +84,33 @@ namespace Neo4jClient.Test.GraphClientTests
                           'properties': 'http://foo/db/data/node/456/properties',
                           'property': 'http://foo/db/data/node/456/property/{key}',
                           'traverse': 'http://foo/db/data/node/456/traverse/{returnType}'
-                        }".Replace('\'', '"')
+                        }")
+                    },
+                    {
+                        MockRequest.PutObjectAsJson("/node/456/properties", nodeToUpdate),
+                        MockResponse.Http((int)HttpStatusCode.NoContent)
+                    },
+                    {
+                        MockRequest.PostObjectAsJson("/index/node/foo", new { key="foo", value="bar", uri="http://foo/db/data/node/456"}),
+                        MockResponse.Json(HttpStatusCode.Created, "Location: http://foo/db/data/index/node/foo/bar/456")
+                    },
+                    {
+                        MockRequest.Delete("/index/node/foo/456"),
+                        MockResponse.Http((int)HttpStatusCode.NoContent)
                     }
-                },
-                {
-                    new RestRequest {
-                        Resource = "/node/456/properties",
-                        Method = Method.PUT,
-                        RequestFormat = DataFormat.Json
-                    }.AddBody(nodeToUpdate),
-                    new NeoHttpResponse {
-                        StatusCode = HttpStatusCode.NoContent
-                    }
-                }, 
-                {
-                    new RestRequest("/index/node/foo", Method.POST) {
-                        RequestFormat = DataFormat.Json,
-                        JsonSerializer = new CustomJsonSerializer { NullHandling = NullValueHandling.Ignore }
-                    }
-                    .AddBody(new { key="foo", value="bar", uri="http://foo/db/data/node/456"}),
-                    new NeoHttpResponse {
-                        StatusCode = HttpStatusCode.Created,
-                        ContentType = "application/json",
-                        TestContent = "Location: http://foo/db/data/index/node/foo/foo/bar/456"
-                    }
-                },
-                {
-                   new RestRequest("/index/node/foo/456", Method.DELETE) {
-                        RequestFormat = DataFormat.Json,
-                        JsonSerializer = new CustomJsonSerializer { NullHandling = NullValueHandling.Ignore }
-                        },
-                    new NeoHttpResponse {
-                        StatusCode = HttpStatusCode.NoContent,
-                        ContentType = "application/json",
-                    }
-                }
-            });
+                })
+            {
+                var graphClient = testHarness.CreateAndConnectGraphClient();
 
-            var graphClient = new GraphClient(new Uri("http://foo/db/data"), httpFactory);
-            graphClient.Connect();
-
-            var pocoReference = new NodeReference<TestNode>(456);
-            graphClient.Update(
-                pocoReference, nodeFromDb =>
-                {
-                    nodeFromDb.Foo = "fooUpdated";
-                    nodeFromDb.Baz = "bazUpdated";
-                    nodeToUpdate = nodeFromDb;
-                }, nodeFromDb => new List<IndexEntry>
+                // Act
+                var pocoReference = new NodeReference<TestNode>(456);
+                graphClient.Update(
+                    pocoReference, nodeFromDb =>
+                    {
+                        nodeFromDb.Foo = "fooUpdated";
+                        nodeFromDb.Baz = "bazUpdated";
+                        nodeToUpdate = nodeFromDb;
+                    }, nodeFromDb => new List<IndexEntry>
                     {
                         new IndexEntry
                             {
@@ -180,32 +119,25 @@ namespace Neo4jClient.Test.GraphClientTests
                             }
                     });
 
-            Assert.AreEqual("fooUpdated", nodeToUpdate.Foo);
-            Assert.AreEqual("bazUpdated", nodeToUpdate.Baz);
-            Assert.AreEqual("bar", nodeToUpdate.Bar);
+                Assert.AreEqual("fooUpdated", nodeToUpdate.Foo);
+                Assert.AreEqual("bazUpdated", nodeToUpdate.Baz);
+                Assert.AreEqual("bar", nodeToUpdate.Bar);
+            }
         }
 
         [Test]
         [ExpectedException(typeof(NotSupportedException))]
         public void ShouldThrowNotSupportedExceptionForPre15M02DatabaseWithIndexEntries()
         {
-            var httpFactory = MockHttpFactory.Generate("http://foo/db/data", new Dictionary<IRestRequest, IHttpResponse>
-            {
+            using (var testHarness = new RestTestHarness
                 {
-                    new RestRequest { Resource = "", Method = Method.GET },
-                    new NeoHttpResponse
                     {
-                        StatusCode = HttpStatusCode.OK,
-                        ContentType = "application/json",
-                        TestContent = pre15M02RootResponse
-                    }
-                },
-                {
-                    new RestRequest { Resource = "/node/456", Method = Method.GET },
-                    new NeoHttpResponse {
-                        StatusCode = HttpStatusCode.OK,
-                        ContentType = "application/json",
-                        TestContent = @"{ 'self': 'http://foo/db/data/node/456',
+                        MockRequest.Get(""),
+                        MockResponse.NeoRootPre15M02()
+                    },
+                    {
+                        MockRequest.Get("/node/456"),
+                        MockResponse.Json(HttpStatusCode.OK, @"{ 'self': 'http://foo/db/data/node/456',
                             'data': { 'Foo': 'foo',
                                     'Bar': 'bar',
                                     'Baz': 'baz'
@@ -220,23 +152,22 @@ namespace Neo4jClient.Test.GraphClientTests
                             'properties': 'http://foo/db/data/node/456/properties',
                             'property': 'http://foo/db/data/node/456/property/{key}',
                             'traverse': 'http://foo/db/data/node/456/traverse/{returnType}'
-                        }".Replace('\'', '"')
-                    }
-                }
-            });
+                        }")
+                    },
+                })
+            {
+                var graphClient = testHarness.CreateAndConnectGraphClient();
 
-            var graphClient = new GraphClient(new Uri("http://foo/db/data"), httpFactory);
-            graphClient.Connect();
-
-            var pocoReference = new NodeReference<TestNode>(456);
-            graphClient.Update(
-                pocoReference,
-                nodeFromDb =>
-                {
-                    nodeFromDb.Foo = "fooUpdated";
-                    nodeFromDb.Baz = "bazUpdated";
-                },
-                nodeFromDb => new List<IndexEntry>
+                //Act
+                var pocoReference = new NodeReference<TestNode>(456);
+                graphClient.Update(
+                    pocoReference,
+                    nodeFromDb =>
+                    {
+                        nodeFromDb.Foo = "fooUpdated";
+                        nodeFromDb.Baz = "bazUpdated";
+                    },
+                    nodeFromDb => new List<IndexEntry>
                 {
                     new IndexEntry
                     {
@@ -244,6 +175,7 @@ namespace Neo4jClient.Test.GraphClientTests
                         KeyValues = new Dictionary<string, object> {{"foo", "bar"}},
                     }
                 });
+            }
         }
 
         [Test]
@@ -251,23 +183,11 @@ namespace Neo4jClient.Test.GraphClientTests
         {
             var nodeToUpdate = new TestNode { Id = 1, Foo = "foo", Bar = "bar", Baz = "baz" };
 
-            var httpFactory = MockHttpFactory.Generate("http://foo/db/data", new Dictionary<IRestRequest, IHttpResponse>
-            {
+            using (var testHarness = new RestTestHarness
                 {
-                    new RestRequest { Resource = "", Method = Method.GET },
-                    new NeoHttpResponse
                     {
-                        StatusCode = HttpStatusCode.OK,
-                        ContentType = "application/json",
-                        TestContent = rootResponse
-                    }
-                },
-                 {
-                    new RestRequest { Resource = "/node/456", Method = Method.GET },
-                    new NeoHttpResponse {
-                        StatusCode = HttpStatusCode.OK,
-                        ContentType = "application/json",
-                        TestContent = @"{ 'self': 'http://foo/db/data/node/456',
+                        MockRequest.Get("/node/456"),
+                        MockResponse.Json(HttpStatusCode.OK, @"{ 'self': 'http://foo/db/data/node/456',
                           'data': { 'Foo': 'foo',
                                     'Bar': 'bar',
                                     'Baz': 'baz'
@@ -282,39 +202,33 @@ namespace Neo4jClient.Test.GraphClientTests
                           'properties': 'http://foo/db/data/node/456/properties',
                           'property': 'http://foo/db/data/node/456/property/{key}',
                           'traverse': 'http://foo/db/data/node/456/traverse/{returnType}'
-                        }".Replace('\'', '"')
+                        }")
+                    },
+                                        {
+                        MockRequest.PutObjectAsJson("/node/456/properties", nodeToUpdate),
+                        MockResponse.Http((int)HttpStatusCode.NoContent)
                     }
-                },
-                {
-                    new RestRequest {
-                        Resource = "/node/456/properties",
-                        Method = Method.PUT,
-                        RequestFormat = DataFormat.Json
-                    }.AddBody(nodeToUpdate),
-                    new NeoHttpResponse {
-                        StatusCode = HttpStatusCode.NoContent
-                    }
-                }
-            });
+                })
+            {
+                var graphClient = testHarness.CreateAndConnectGraphClient();
 
-            var graphClient = new GraphClient(new Uri("http://foo/db/data"), httpFactory);
-            graphClient.Connect();
+                //Act
+                var hasChanged = false;
 
-            var hasChanged = false;
+                var pocoReference = new NodeReference<TestNode>(456);
+                graphClient.Update(
+                    pocoReference, nodeFromDb =>
+                    {
+                        nodeFromDb.Foo = "fooUpdated";
+                        nodeFromDb.Baz = "bazUpdated";
+                        nodeToUpdate = nodeFromDb;
+                    },
+                    null,
+                    diff => { hasChanged = diff.Any(); }
+                    );
 
-            var pocoReference = new NodeReference<TestNode>(456);
-            graphClient.Update(
-                pocoReference, nodeFromDb =>
-                {
-                    nodeFromDb.Foo = "fooUpdated";
-                    nodeFromDb.Baz = "bazUpdated";
-                    nodeToUpdate = nodeFromDb;
-                },
-                null,
-                diff => { hasChanged = diff.Any(); }
-                );
-
-            Assert.IsTrue(hasChanged);
+                Assert.IsTrue(hasChanged);
+            }
         }
 
         public class TestNode
