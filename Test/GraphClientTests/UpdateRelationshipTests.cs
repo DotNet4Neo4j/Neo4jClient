2508328bde5e8ce93dc228f79d039b2c73b5a747
Tatham Oddie <tatham@oddie.com.au>
2012-08-28 16:15:28 +1000

    Refactor test

--- Test/GraphClientTests/UpdateRelationshipTests.cs
+++ Test/GraphClientTests/UpdateRelationshipTests.cs
@@ -1,6 +1,5 @@
 ﻿using System.Net;
 using NUnit.Framework;
-using RestSharp;
 
 namespace Neo4jClient.Test.GraphClientTests
 {
@@ -10,113 +9,61 @@ namespace Neo4jClient.Test.GraphClientTests
         [Test]
         public void ShouldUpdatePayload()
         {
-            var testHarness = new RestTestHarness
+            using (var testHarness = new RestTestHarness
             {
                 {
-                    MockRequest.Get(""),
-                    new NeoHttpResponse
-                    {
-                        StatusCode = HttpStatusCode.OK,
-                        ContentType = "application/json",
-                        TestContent = @"{
-                          'batch' : 'http://foo/db/data/batch',
-                          'node' : 'http://foo/db/data/node',
-                          'node_index' : 'http://foo/db/data/index/node',
-                          'relationship_index' : 'http://foo/db/data/index/relationship',
-                          'reference_node' : 'http://foo/db/data/node/0',
-                          'extensions_info' : 'http://foo/db/data/ext',
-                          'extensions' : {
-                          }
-                        }".Replace('\'', '"')
-                    }
-                },
-                 {
-                    new NeoHttpRequest { Resource = "/relationship/456/properties", Method = Method.GET },
-                    new NeoHttpResponse {
-                        StatusCode = HttpStatusCode.OK,
-                        ContentType = "application/json",
-                        TestContent = @"{ 'Foo': 'foo', 'Bar': 'bar', 'Baz': 'baz' }".Replace('\'', '"')
-                    }
+                    MockRequest.Get("/relationship/456/properties"),
+                    MockResponse.Json(HttpStatusCode.OK, "{ 'Foo': 'foo', 'Bar': 'bar', 'Baz': 'baz' }")
                 },
                 {
-                    new NeoHttpRequest {
-                        Resource = "/relationship/456/properties",
-                        Method = Method.PUT,
-                        RequestFormat = DataFormat.Json,
-                        Body = new TestPayload { Foo = "fooUpdated", Bar = "bar", Baz = "bazUpdated" }
-                    },
-                    new NeoHttpResponse {
-                        StatusCode = HttpStatusCode.NoContent
-                    }
+                    MockRequest.PutObjectAsJson(
+                        "/relationship/456/properties",
+                        new TestPayload { Foo = "fooUpdated", Bar = "bar", Baz = "bazUpdated" }),
+                    MockResponse.Http((int)HttpStatusCode.NoContent)
                 }
-            };
+            })
+            {
+                var graphClient = testHarness.CreateAndConnectGraphClient();
 
-            var graphClient = testHarness.CreateAndConnectGraphClient();
-
-            graphClient.Update(
-                new RelationshipReference<TestPayload>(456),
-                payloadFromDb =>
-                {
-                    payloadFromDb.Foo = "fooUpdated";
-                    payloadFromDb.Baz = "bazUpdated";
-                }
-            );
-
-            testHarness.AssertAllRequestsWereReceived();
+                graphClient.Update(
+                    new RelationshipReference<TestPayload>(456),
+                    payloadFromDb =>
+                        {
+                            payloadFromDb.Foo = "fooUpdated";
+                            payloadFromDb.Baz = "bazUpdated";
+                        }
+                    );
+            }
         }
 
         [Test]
         public void ShouldInitializePayloadDuringUpdate()
         {
-            var testHarness = new RestTestHarness
+            using (var testHarness = new RestTestHarness
             {
                 {
-                    MockRequest.Get(""),
-                    new NeoHttpResponse
+                    MockRequest.Get("/relationship/456/properties"),
+                    MockResponse.Http((int)HttpStatusCode.NoContent)
+                },
+                {
+                    MockRequest.PutObjectAsJson(
+                        "/relationship/456/properties",
+                        new TestPayload { Foo = "fooUpdated", Baz = "bazUpdated" }),
+                    MockResponse.Http((int)HttpStatusCode.NoContent)
+                }
+            })
+            {
+                var graphClient = testHarness.CreateAndConnectGraphClient();
+
+                graphClient.Update(
+                    new RelationshipReference<TestPayload>(456),
+                    payloadFromDb =>
                     {
-                        StatusCode = HttpStatusCode.OK,
-                        ContentType = "application/json",
-                        TestContent = @"{
-                          'batch' : 'http://foo/db/data/batch',
-                          'node' : 'http://foo/db/data/node',
-                          'node_index' : 'http://foo/db/data/index/node',
-                          'relationship_index' : 'http://foo/db/data/index/relationship',
-                          'reference_node' : 'http://foo/db/data/node/0',
-                          'extensions_info' : 'http://foo/db/data/ext',
-                          'extensions' : {
-                          }
-                        }".Replace('\'', '"')
+                        payloadFromDb.Foo = "fooUpdated";
+                        payloadFromDb.Baz = "bazUpdated";
                     }
-                },
-                 {
-                    new NeoHttpRequest { Resource = "/relationship/456/properties", Method = Method.GET },
-                    new NeoHttpResponse { StatusCode = HttpStatusCode.NoContent }
-                },
-                {
-                    new NeoHttpRequest {
-                        Resource = "/relationship/456/properties",
-                        Method = Method.PUT,
-                        RequestFormat = DataFormat.Json,
-                        Body = new TestPayload { Foo = "fooUpdated", Bar = "", Baz = "bazUpdated" }
-                    },
-                    new NeoHttpResponse {
-                        StatusCode = HttpStatusCode.NoContent
-                    }
-                }
-            };
-
-            var graphClient = testHarness.CreateAndConnectGraphClient();
-
-            graphClient.Update(
-                new RelationshipReference<TestPayload>(456),
-                payloadFromDb =>
-                {
-                    payloadFromDb.Foo = "fooUpdated";
-                    payloadFromDb.Baz = "bazUpdated";
-                }
-            );
-
-            testHarness.AssertAllRequestsWereReceived();
+                    );
+            }
         }
 
         public class TestPayload
