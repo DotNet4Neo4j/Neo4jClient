f4930f4701cd14671e11887f5b700446119a61ae
Tatham Oddie <tatham@oddie.com.au>
2012-09-19 09:22:50 +1000

    Split Cypher DELETE tests into their own fixture

--- Test/Cypher/CypherFluentQueryTests.cs
+++ Test/Cypher/CypherFluentQueryTests.cs
@@ -991,57 +991,6 @@ RETURN b AS NodeB";
         }
 
         [Test]
-        public void DeleteMatchedIdentifier()
-        {
-            // http://docs.neo4j.org/chunked/milestone/query-delete.html#delete-remove-a-node-and-connected-relationships
-            // START n = node(3)
-            // MATCH n-[r]-()
-            // DELETE n, r
-
-            var client = Substitute.For<IRawGraphClient>();
-            var query = new CypherFluentQuery(client)
-                .Start("n", (NodeReference)3)
-                .Match("n-[r]-()")
-                .Delete("n, r")
-                .Query;
-
-            Assert.AreEqual("START n=node({p0})\r\nMATCH n-[r]-()\r\nDELETE n, r", query.QueryText);
-            Assert.AreEqual(3, query.QueryParameters["p0"]);
-        }
-
-        [Test]
-        public void DeleteProperty()
-        {
-            // http://docs.neo4j.org/chunked/1.8.M06/query-delete.html#delete-remove-a-property
-            //START andres = node(3)
-            //DELETE andres.age
-            //RETURN andres
-
-            var client = Substitute.For<IRawGraphClient>();
-            var query = new CypherFluentQuery(client)
-                .Start("andres", (NodeReference)3)
-                .Delete("andres.age")
-                .Return<Node<Object>>("andres")
-                .Query;
-
-            Assert.AreEqual("START andres=node({p0})\r\nDELETE andres.age\r\nRETURN andres", query.QueryText);
-            Assert.AreEqual(3, query.QueryParameters["p0"]);
-        }
-
-        [Test]
-        public void DeleteIdentifier()
-        {
-            var client = Substitute.For<IRawGraphClient>();
-            var query = new CypherFluentQuery(client)
-                .Start("n", (NodeReference)3)
-                .Delete("n")
-                .Query;
-
-            Assert.AreEqual("START n=node({p0})\r\nDELETE n", query.QueryText);
-            Assert.AreEqual(3, query.QueryParameters["p0"]);
-        }
-
-        [Test]
         public void ComplexMatching()
         {
             // http://docs.neo4j.org/chunked/1.8.M03/query-match.html#match-complex-matching
