b6ef85a27dbf756604a16d2b4e871a11a4fb2e81
Tatham Oddie <tatham@oddie.com.au>
2012-11-09 10:25:26 +1000

    Stub new Cypher.QueryWriter

---
+++ Neo4jClient/Cypher/QueryWriter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Neo4jClient.Cypher
+{
+    public class QueryWriter
+    {
+        readonly IDictionary<string, object> queryParameters = new Dictionary<string, object>();
+        readonly StringBuilder queryTextBuilder = new StringBuilder();
+
+        public CypherQuery ToCypherQuery()
+        {
+            var queryText = queryTextBuilder
+                .ToString()
+                .TrimEnd(Environment.NewLine.ToCharArray());
+
+            return new CypherQuery(
+                queryText,
+                queryParameters,
+                CypherResultMode.Projection);
+        }
+
+        public void AppendClause(string clause, params object[] paramValues)
+        {
+            if (paramValues.Any())
+            {
+                var paramPlaceholders = paramValues
+                    .Select(CreateParameterAndReturnPlaceholder)
+                    .Cast<object>()
+                    .ToArray();
+                clause = string.Format(clause, paramPlaceholders);
+            }
+
+            queryTextBuilder.AppendLine(clause);
+        }
+
+        string CreateParameterAndReturnPlaceholder(object paramValue)
+        {
+            var paramName = string.Format("p{0}", queryParameters.Count);
+            queryParameters.Add(paramName, paramValue);
+            return "{" + paramName + "}";
+        }
+    }
+}
---
+++ Test/Cypher/QueryWriterTests.cs
@@ -0,0 +1,76 @@
+﻿using NUnit.Framework;
+using Neo4jClient.Cypher;
+
+namespace Neo4jClient.Test.Cypher
+{
+    [TestFixture]
+    public class QueryWriterTests
+    {
+        [Test]
+        public void EmptyQueryForNoClauses()
+        {
+            var writer = new QueryWriter();
+
+            var query = writer.ToCypherQuery();
+            Assert.AreEqual("", query.QueryText);
+            Assert.AreEqual(0, query.QueryParameters.Count);
+        }
+
+        [Test]
+        public void AppendClause()
+        {
+            var writer = new QueryWriter();
+
+            writer.AppendClause("foo");
+
+            var query = writer.ToCypherQuery();
+            Assert.AreEqual("foo", query.QueryText);
+            Assert.AreEqual(0, query.QueryParameters.Count);
+        }
+
+        [Test]
+        public void AppendMultipleClauses()
+        {
+            var writer = new QueryWriter();
+
+            writer.AppendClause("foo");
+            writer.AppendClause("bar");
+
+            var query = writer.ToCypherQuery();
+            Assert.AreEqual("foo\r\nbar", query.QueryText);
+            Assert.AreEqual(0, query.QueryParameters.Count);
+        }
+
+        [Test]
+        public void AppendClauseWithParameter()
+        {
+            var writer = new QueryWriter();
+
+            writer.AppendClause(
+                "foo {0}",
+                "bar");
+
+            var query = writer.ToCypherQuery();
+            Assert.AreEqual("foo {p0}", query.QueryText);
+            Assert.AreEqual(1, query.QueryParameters.Count);
+            Assert.AreEqual("bar", query.QueryParameters["p0"]);
+        }
+
+        [Test]
+        public void AppendClauseWithMultipleParameters()
+        {
+            var writer = new QueryWriter();
+
+            writer.AppendClause(
+                "foo {0} bar {1}",
+                "baz",
+                "qak");
+
+            var query = writer.ToCypherQuery();
+            Assert.AreEqual("foo {p0} bar {p1}", query.QueryText);
+            Assert.AreEqual(2, query.QueryParameters.Count);
+            Assert.AreEqual("baz", query.QueryParameters["p0"]);
+            Assert.AreEqual("qak", query.QueryParameters["p1"]);
+        }
+    }
+}
