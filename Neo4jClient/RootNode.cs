6980c66bf3c2b37665b720927dbc1ca40f8f6378
Tatham Oddie <tatham@oddie.com.au>
2012-12-05 14:33:53 +1100

    Remove redundant member that hides an identical implementation

--- Neo4jClient/RootNode.cs
+++ Neo4jClient/RootNode.cs
@@ -7,13 +7,5 @@ namespace Neo4jClient
         public RootNode() : base(0) { }
         public RootNode(long id) : base(id) {}
         public RootNode(long id, IGraphClient client) : base(id, client) {}
-
-        public ICypherFluentQueryStarted StartCypher(string identity)
-        {
-            var client = ((IAttachedReference)this).Client;
-            var query = new CypherFluentQuery(client)
-                .AddStartPoint(identity, this);
-            return query;
-        }
     }
 }
