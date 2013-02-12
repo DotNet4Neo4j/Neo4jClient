50e99a2c0b412c9ffcd0d93b32cedc5346766cf5
Tatham Oddie <tatham@oddie.com.au>
2013-02-12 20:40:29 +1100

    Change CollectAs to return an IEnumerable by default, like it should have always done

--- Neo4jClient/Cypher/ICypherResultItem.cs
+++ Neo4jClient/Cypher/ICypherResultItem.cs
@@ -1,9 +1,11 @@
+using System.Collections.Generic;
+
 namespace Neo4jClient.Cypher
 {
     public interface ICypherResultItem
     {
         T As<T>();
-        T CollectAs<T>();
+        IEnumerable<T> CollectAs<T>();
         Node<T> Node<T>();
     }
 }
