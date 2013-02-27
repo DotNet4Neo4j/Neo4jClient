24d7f65fb10f796a721e91f47a542b90f370f4c0
Tatham Oddie <tatham@oddie.com.au>
2013-02-27 18:44:34 +1100

    Add test for With clause

---
+++ Test/Cypher/CypherFluentQueryWithTests.cs
@@ -0,0 +1,23 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using Neo4jClient.Cypher;
+
+namespace Neo4jClient.Test.Cypher
+{
+    [TestFixture]
+    public class CypherFluentQueryWithTests
+    {
+        [Test]
+        public void With()
+        {
+            var client = Substitute.For<IRawGraphClient>();
+            var query = new CypherFluentQuery(client)
+                .Start("n", (NodeReference)3)
+                .With("foo")
+                .Query;
+
+            Assert.AreEqual("START n=node({p0})\r\nWITH foo", query.QueryText);
+            Assert.AreEqual(3, query.QueryParameters["p0"]);
+        }
+    }
+}
