0ec559720c1884d8f2da636d3281988a05661619
Tatham Oddie <tatham@oddie.com.au>
2013-03-25 14:04:52 +1100

    Add end-to-end test for Return statements using anonymous types

--- Test/Cypher/CypherFluentQueryReturnTests.cs
+++ Test/Cypher/CypherFluentQueryReturnTests.cs
@@ -1,5 +1,9 @@
-﻿using NSubstitute;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using NSubstitute;
 using NUnit.Framework;
+using Neo4jClient.ApiModels.Cypher;
 using Neo4jClient.Cypher;
 
 namespace Neo4jClient.Test.Cypher
@@ -140,6 +144,59 @@ ORDER BY common.FirstName", query.QueryText);
                 .Query;
 
             Assert.AreEqual(CypherResultMode.Projection, query.ResultMode);
+        }
+
+        [Test]
+        public void ShouldSupportAnonymousReturnTypesEndToEnd()
+        {
+            const string queryText = "START root=node({p0})\r\nMATCH root-->other\r\nRETURN other AS Foo";
+            var parameters = new Dictionary<string, object>
+            {
+                {"p0", 123}
+            };
+
+            var cypherQuery = new CypherQuery(queryText, parameters, CypherResultMode.Projection);
+            var cypherApiQuery = new CypherApiQuery(cypherQuery);
+
+            using (var testHarness = new RestTestHarness
+            {
+                {
+                    MockRequest.PostObjectAsJson("/cypher", cypherApiQuery),
+                    MockResponse.Json(HttpStatusCode.OK, @"{
+                        'data' : [
+                            [ 1 ],
+                            [ 2 ],
+                            [ 3 ]
+                        ],
+                        'columns' : [ 'Foo' ]
+                    }")
+                }
+            })
+            {
+                var graphClient = testHarness.CreateAndConnectGraphClient();
+
+                var results = graphClient
+                    .Cypher
+                    .Start("root", graphClient.RootNode)
+                    .Match("root-->other")
+                    .Return(other => new
+                    {
+                        Foo = other.As<int>()
+                    })
+                    .Results
+                    .ToList();
+
+                Assert.AreEqual(3, results.Count());
+
+                var result = results[0];
+                Assert.AreEqual(1, result.Foo);
+
+                result = results[1];
+                Assert.AreEqual(2, result.Foo);
+
+                result = results[2];
+                Assert.AreEqual(3, result.Foo);
+            }
         }
     }
 }
