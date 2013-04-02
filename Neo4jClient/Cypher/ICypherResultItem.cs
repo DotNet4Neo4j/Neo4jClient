d1f27d8fbf2bfbbe1415e5fbfef8e4728c48fc37
Tatham Oddie <tatham@oddie.com.au>
2013-04-03 01:28:27 +1100

    Document existing methods

--- Neo4jClient/Cypher/ICypherResultItem.cs
+++ Neo4jClient/Cypher/ICypherResultItem.cs
@@ -4,9 +4,26 @@ namespace Neo4jClient.Cypher
 {
     public interface ICypherResultItem
     {
+        /// <summary>
+        /// Does not emit anything into the query; purely used for design-time type coersion in C#
+        /// </summary>
         T As<T>();
-        IEnumerable<Node<T>> CollectAs<T>();
-        IEnumerable<Node<T>> CollectAsDistinct<T>();
+
+        /// <summary>
+        /// Shortcut for As&lt;Node&lt;T&gt;&gt;
+        /// </summary>
         Node<T> Node<T>();
+
+        /// <summary>
+        /// Equivalent to <code>RETURN collect(foo)</code>
+        /// http://docs.neo4j.org/chunked/stable/query-function.html#functions-collect
+        /// </summary>
+        IEnumerable<Node<T>> CollectAs<T>();
+
+        /// <summary>
+        /// Equivalent to <code>RETURN collect(distinct foo)</code>
+        /// http://docs.neo4j.org/chunked/stable/query-function.html#functions-collect
+        /// </summary>
+        IEnumerable<Node<T>> CollectAsDistinct<T>();
     }
 }
