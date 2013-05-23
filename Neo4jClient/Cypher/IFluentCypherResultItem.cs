df0b1082f6b2e991d6e469c6b9cf0de73d9f0623
Tatham Oddie <tatham@oddie.com.au>
2013-05-23 12:30:06 +1000

    Remove redundant imports

--- Neo4jClient/Cypher/IFluentCypherResultItem.cs
+++ Neo4jClient/Cypher/IFluentCypherResultItem.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Neo4jClient.Cypher
+﻿namespace Neo4jClient.Cypher
 {
     public interface IFluentCypherResultItem
     {
