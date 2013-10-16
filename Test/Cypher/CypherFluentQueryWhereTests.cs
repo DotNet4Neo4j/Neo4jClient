77ddda38d8181361b8f619c1ce649082ba9ad7b4
Tatham Oddie <tatham@oddie.com.au>
2013-10-16 11:44:26 +1100

    Add test for nesting of where predicates

--- Test/Cypher/CypherFluentQueryWhereTests.cs
+++ Test/Cypher/CypherFluentQueryWhereTests.cs
@@ -39,5 +39,18 @@ namespace Neo4jClient.Test.Cypher
             Assert.AreEqual("WHERE (a.Bar <> b.Bar)", query.QueryText);
             Assert.AreEqual(0, query.QueryParameters.Count);
         }
+
+        [Test]
+        public void NestOrAndAndCorrectly()
+        {
+            var client = Substitute.For<IRawGraphClient>();
+            var query = new CypherFluentQuery(client)
+                .Where((Foo a, Foo b) => a.Bar == 123 || b.Bar == 456)
+                .AndWhere((Foo c) => c.Bar == 789)
+                .Query;
+
+            Assert.AreEqual("WHERE ((a.Bar = {p0}) OR (b.Bar = {p1}))\r\nAND (c.Bar = {p2})", query.QueryText);
+            Assert.AreEqual(3, query.QueryParameters.Count);
+        }
     }
 }
