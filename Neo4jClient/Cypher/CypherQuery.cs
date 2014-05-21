7369fc6957d13e158112cdb0d33e122ac1b6c8d1
Tatham Oddie <tatham@oddie.com.au>
2014-05-21 20:05:41 +1000

    Remove redundant import

--- Neo4jClient/Cypher/CypherQuery.cs
+++ Neo4jClient/Cypher/CypherQuery.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text.RegularExpressions;
 using Neo4jClient.Serialization;
 
 namespace Neo4jClient.Cypher
