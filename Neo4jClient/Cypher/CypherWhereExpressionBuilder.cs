2890c29c518f9212dd38826f82bdf1745eb7356b
Simon Pinn <simon@spinn.net.au>
2014-08-29 08:30:01 +1000

    Apply camel casing to where expression builder

--- Neo4jClient/Cypher/CypherWhereExpressionBuilder.cs
+++ Neo4jClient/Cypher/CypherWhereExpressionBuilder.cs
@@ -8,7 +8,7 @@ namespace Neo4jClient.Cypher
         public static string BuildText(
             LambdaExpression expression,
             Func<object, string> createParameterCallback,
-            CypherCapabilities capabilities = null)
+            CypherCapabilities capabilities = null, bool camelCaseProperties = false)
         {
             capabilities = capabilities ?? CypherCapabilities.Default;
 
@@ -16,7 +16,7 @@ namespace Neo4jClient.Cypher
                 expression.Body.NodeType == ExpressionType.MemberAccess)
                 throw new NotSupportedException("Member access expressions, like Where(f => f.Foo), are not supported because these become ambiguous between C# and Cypher based on how Neo4j handles null values. Use a comparison instead, like Where(f => f.Foo == true).");
 
-            var myVisitor = new CypherWhereExpressionVisitor(createParameterCallback, capabilities);
+            var myVisitor = new CypherWhereExpressionVisitor(createParameterCallback, capabilities,camelCaseProperties);
             myVisitor.Visit(expression);
             return myVisitor.TextOutput.ToString();
         }
