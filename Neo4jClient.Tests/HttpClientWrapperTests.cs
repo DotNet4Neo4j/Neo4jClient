909c92f357f1f0f377d3cf0f66761daf109be048
Chris Skardon <chris@tournr.com>
2016-03-16 10:05:40 +0000

    Removed Tests That couldn't work
    Due to not being able to Mock the HttpClient properly.

--- Neo4jClient.Tests/HttpClientWrapperTests.cs
+++ Neo4jClient.Tests/HttpClientWrapperTests.cs
@@ -15,6 +15,31 @@ namespace Neo4jClient.Test
         }
 
         [Test]
+        public void CreatesNewHttpClientWhenNotSupplied()
+        {
+            var wrapper = new HttpClientWrapper();
+            Assert.IsNotNull(wrapper.Client);
+        }
+
+        [Test]
+        public void DoesNotCreateAuthenticationHeaderWhenPasswordIsNullOrEmpty(
+            [Values("", " ", null)] string password
+            )
+        {
+            var wrapper = new HttpClientWrapper(password: password);
+            Assert.IsNull(wrapper.AuthenticationHeaderValue);
+        }
+
+        [Test]
+        public void DoesNotCreateAuthenticationHeaderWhenUsernameIsNullOrEmpty(
+            [Values("", " ", null)] string username
+            )
+        {
+            var wrapper = new HttpClientWrapper(username);
+            Assert.IsNull(wrapper.AuthenticationHeaderValue);
+        }
+
+        [Test]
         public void SetsAuthenticationHeaderValueCorrectly()
         {
             const string username = "user";
@@ -27,40 +52,6 @@ namespace Neo4jClient.Test
         }
 
         [Test]
-        public void CreatesNewHttpClientWhenNotSupplied()
-        {
-            var wrapper = new HttpClientWrapper();
-            Assert.IsNotNull(wrapper.Client);
-        }
-
-        [Test]
-        public void DoesNotCreateAuthenticationHeaderWhenUsernameIsNullOrEmpty(
-            [Values("", " ", null)]string username
-            )
-        {
-            var wrapper = new HttpClientWrapper(username);
-            Assert.IsNull(wrapper.AuthenticationHeaderValue);
-        }
-
-        [Test]
-        public void DoesNotCreateAuthenticationHeaderWhenPasswordIsNullOrEmpty(
-            [Values("", " ", null)]string password
-            )
-        {
-            var wrapper = new HttpClientWrapper(password:password);
-            Assert.IsNull(wrapper.AuthenticationHeaderValue);
-        }
-
-        [Test]
-        public void UsesGivenHttpClient()
-        {
-            var client = Substitute.For<HttpClient>();
-            var wrapper = new HttpClientWrapper(client);
-
-            Assert.AreSame(wrapper.Client, client);
-        }
-
-        [Test]
         public void SetsTheUsernameAndPasswordProperties()
         {
             const string username = "username";
@@ -70,33 +61,14 @@ namespace Neo4jClient.Test
             Assert.AreEqual(username, wrapper.Username);
             Assert.AreEqual(password, wrapper.Password);
         }
-        
 
         [Test]
-        public void AddsAuthorizationHeaderToRequestWhenSet()
-        {
-            const string username = "username";
-            const string password = "password";
-            var client = Substitute.For<HttpClient>();
-            var wrapper = new HttpClientWrapper(username, password, client);
-
-            var requestWithAuth = new HttpRequestMessage(HttpMethod.Get, "http://foo/");
-            requestWithAuth.Headers.Authorization = wrapper.AuthenticationHeaderValue;
-
-            wrapper.SendAsync(new HttpRequestMessage(HttpMethod.Get, "http://foo/"));
-            client.Received(1).SendAsync(requestWithAuth);
-        }
-
-        [Test]
-        public void DoesNotAddAuthorizationHeaderToRequestWhenNotSet()
+        public void UsesGivenHttpClient()
         {
             var client = Substitute.For<HttpClient>();
-            var wrapper = new HttpClientWrapper(client: client);
+            var wrapper = new HttpClientWrapper(client);
 
-            var request = new HttpRequestMessage(HttpMethod.Get, "http://foo/");
-
-            wrapper.SendAsync(request);
-            client.Received(1).SendAsync(request);
+            Assert.AreSame(wrapper.Client, client);
         }
     }
 }
