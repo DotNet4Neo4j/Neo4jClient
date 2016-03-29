2e33ba1579bda9800146ecc9f52a029e764a736d
Ross Jones <rossj@trackmatic.co.za>
2016-03-29 17:45:23 +0200

    Expose return distinct which allows user to provide expression
    When developing libraries on top of Neo4jClient it can be useful to
    expose the low level API to allow control over the return statement
    without having to dynamically build up expression trees

--- Neo4jClient/Cypher/CypherFluentQuery`Return.cs
+++ Neo4jClient/Cypher/CypherFluentQuery`Return.cs
@@ -45,6 +45,16 @@ namespace Neo4jClient.Cypher
             throw new NotSupportedException("This was an internal that never should have been exposed. If you want to create a projection, you should be using the lambda overload instead. See the 'Using Functions in Return Clauses' and 'Using Custom Text in Return Clauses' sections of https://bitbucket.org/Readify/neo4jclient/wiki/cypher for details of how to do this.");
         }
 
+        public ICypherFluentQuery<TResult> ReturnDistinct<TResult>(ReturnExpression expression)
+        {
+            return Mutate<TResult>(w =>
+            {
+                w.ResultMode = expression.ResultMode;
+                w.ResultFormat = expression.ResultFormat;
+                w.AppendClause("RETURN distinct " + expression.Text);
+            });
+        }
+
         public ICypherFluentQuery<TResult> ReturnDistinct<TResult>(string identity)
         {
             return Mutate<TResult>(w => w.AppendClause("RETURN distinct " + identity));
@@ -65,13 +75,7 @@ namespace Neo4jClient.Cypher
         ICypherFluentQuery<TResult> ReturnDistinct<TResult>(LambdaExpression expression)
         {
             var returnExpression = CypherReturnExpressionBuilder.BuildText(expression, Client.CypherCapabilities, Client.JsonConverters, CamelCaseProperties);
-
-            return Mutate<TResult>(w =>
-            {
-                w.ResultMode = returnExpression.ResultMode;
-                w.ResultFormat = returnExpression.ResultFormat;
-                w.AppendClause("RETURN distinct " + returnExpression.Text);
-            });
+            return ReturnDistinct<TResult>(returnExpression);
         }
 
         public ICypherFluentQuery<TResult> Return<TResult>(Expression<Func<TResult>> expression)
