4464b6c4cde550e36c336b83942346a6696185b7
Arturo Sevilla <arturosevilla@gmail.com>
2018-03-15 12:57:11 -0700

    Fix datetime serialization for native driver parameters

--- Neo4jClient.Shared/Neo4jDriverExtensions.cs
+++ Neo4jClient.Shared/Neo4jDriverExtensions.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Globalization;
 using Neo4j.Driver.V1;
 using Neo4jClient.Cypher;
 using Newtonsoft.Json;
@@ -11,6 +12,8 @@ namespace Neo4jClient
 {
     public static class Neo4jDriverExtensions
     {
+        private const string DefaultDateTimeFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ss.FFFFFFFK";
+
         public static IStatementResult Run(this ISession session, CypherQuery query, IGraphClient gc)
         {
             return session.Run(query.QueryText, query.ToNeo4jDriverParameters(gc));
@@ -78,6 +81,16 @@ namespace Neo4jClient
 
         private static object SerializePrimitive(Type type, TypeInfo typeInfo, object instance)
         {
+            if (type == typeof(DateTime))
+            {
+                return SerializeDateTime((DateTime) instance);
+            }
+
+            if (type == typeof(DateTimeOffset))
+            {
+                return SerializeDateTimeOffset((DateTimeOffset) instance);
+            }
+
             if (type == typeof(string) || typeInfo.IsPrimitive)
             {
                 return instance;
@@ -88,8 +101,20 @@ namespace Neo4jClient
                 return $"{instance}";
             }
 
+            // last case scenario serialize it as JSON
             return JsonConvert.SerializeObject(instance);
         }
+
+        private static string SerializeDateTime(DateTime dateTime)
+        {
+             return dateTime.ToString(DefaultDateTimeFormat, CultureInfo.CurrentCulture);
+        }
+
+        private static string SerializeDateTimeOffset(DateTimeOffset dateTime)
+        {
+            return dateTime.ToString(DefaultDateTimeFormat, CultureInfo.CurrentCulture);
+        }
+
 
         private static object SerializeDictionary(Type type, object value)
         {
@@ -110,15 +135,6 @@ namespace Neo4jClient
             }
 
             return serialized;
-        }
-
-        private static IEnumerable<IDictionary<string, object>> ConvertListToListOfDictionaries(IEnumerable list, IGraphClient gc)
-        {
-            var output = new List<IDictionary<string, object>>();
-            foreach (var item in list)
-                output.Add(JsonConvert.DeserializeObject<Dictionary<string, object>>(JsonConvert.SerializeObject(item, Formatting.None, gc.JsonConverters.ToArray())));
-
-            return output;
         }
     }
 }
