fd143f8ab7f2b7ac06f0524a3a8304dffe99a54c
Chris Skardon <chris@tournr.com>
2019-03-19 12:49:49 +0000

    Adding `In` and `NotIn` as extension methods for Object
    You can now do: 
    ```
    var years = new [] {1980, 1982, 1984};
    /*.. stuff ..*/
    .Where((Person p) => p.Born.In(years))
    ```
    
    Which will generate:
    `WHERE (p.Born IN {p0})`

---
+++ Neo4jClient.Shared/Extensions/ObjectExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Neo4jClient.Extensions
+{
+    public static class ObjectExtensions
+    {
+
+        public static bool In<T>(this T obj, IEnumerable<T> enumerable)
+        {
+            return enumerable.Contains(obj);
+        }
+
+        public static bool NotIn<T>(this T obj, IEnumerable<T> enumerable)
+        {
+            return !obj.In(enumerable);
+        }
+    }
+}
