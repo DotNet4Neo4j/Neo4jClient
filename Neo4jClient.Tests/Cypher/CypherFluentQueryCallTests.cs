7ca6724706736c1656e5b087b0019660b8b0e5db
Charlotte Skardon <charlotte@tournr.com>
2021-06-21 11:33:00 +0100

    All tests to `\r\n` over `@`

--- Neo4jClient.Tests/Cypher/CypherFluentQueryCallTests.cs
+++ Neo4jClient.Tests/Cypher/CypherFluentQueryCallTests.cs
@@ -70,145 +70,119 @@ namespace Neo4jClient.Tests.Cypher
 
             callQuery.Query.QueryText.Should().Be(expected);
         }
-//
-//         [Fact]
-//         public void Call_SubQueriesAsLambdaWithParameters()
-//         {
-//             const string expected = @"CALL { MATCH (n)
-// WHERE (n.Id = $p0)
-// RETURN count(n) AS c }
-// RETURN c";
-//
-//             var client = GraphClient_30;
-//             var query = new CypherFluentQuery(client)
-//                 .Match("(n)")
-//                 .Where((Foo n) => n.Id == 1)
-//                 .Return(n => new {c = n.Count()});
-//             
-//             var callQuery = new CypherFluentQuery(client)
-//                 .Call(() => query)
-//                 .Return(c => c.As<long>());
-//
-//             callQuery.Query.QueryText.Should().Be(expected);
-//             callQuery.Query.QueryParameters.Should().HaveCount(1);
-//         }
-//
-//         [Fact]
-//         public void Call_SubQueriesAsLambdaWithParametersPriorToCall()
-//         {
-//             const string expected = @"MATCH (x)
-// WHERE (x.Id = $p0)
-// CALL { MATCH (n)
-// WHERE (n.Id = $p1)
-// RETURN count(n) AS c }
-// RETURN c";
-//
-//             var client = GraphClient_30;
-//             var query = new CypherFluentQuery(client)
-//                 .Match("(n)")
-//                 .Where((Foo n) => n.Id == 1)
-//                 .Return(n => new {c = n.Count()});
-//             
-//             var callQuery = new CypherFluentQuery(client)
-//                 .Match("(x)")
-//                 .Where((Foo x) => x.Id == 2)
-//                 .Call(() => query)
-//                 .Return(c => c.As<long>());
-//
-//             callQuery.Query.QueryText.Should().Be(expected);
-//             callQuery.Query.QueryParameters.Should().HaveCount(2);
-//         }
-//
-//         [Fact]
-//         public void Call_SubQueriesAsLambdaWithParametersAfterToCall()
-//         {
-//             const string expected = @"CALL { MATCH (n)
-// WHERE (n.Id = $p0)
-// RETURN count(n) AS c }
-// MATCH (x)
-// WHERE (x.Id = $p1)
-// RETURN c";
-//
-//             var client = GraphClient_30;
-//             var query = new CypherFluentQuery(client)
-//                 .Match("(n)")
-//                 .Where((Foo n) => n.Id == 1)
-//                 .Return(n => new {c = n.Count()});
-//             
-//             var callQuery = new CypherFluentQuery(client)
-//                 .Call(() => query)
-//                 .Match($"(x)")
-//                 .Where((Foo x) => x.Id == 2)
-//                 .Return(c => c.As<long>());
-//
-//             callQuery.Query.QueryText.Should().Be(expected);
-//             callQuery.Query.QueryParameters.Should().HaveCount(2);
-//         }
-//
-//         [Fact]
-//         public void Call_SubQueriesAsLambdaWithParametersPriorToCall_WholeWord()
-//         {
-//             const string expected = @"MATCH (x)
-// WHERE (x.Id = $p0)
-// CALL { MATCH (n)
-// WHERE (n.Id = $p1)
-// AND (n.Something = $p2)
-// RETURN count(n) AS c }
-// RETURN c";
-//
-//             var client = GraphClient_30;
-//             var query = new CypherFluentQuery(client)
-//                 .Match("(n)")
-//                 .Where("(n.Id = $p0)")
-//                 .AndWhere("(n.Something = $p1)")
-//                 .WithParam("p0", 1)
-//                 .WithParam("p1", 2)
-//                 .Return(n => new {c = n.Count()});
-//             
-//             var callQuery = new CypherFluentQuery(client)
-//                 .Match("(x)")
-//                 .Where("(x.Id = $p0)")
-//                 .WithParam("p0", 3)
-//                 .Call(() => query)
-//                 .Return(c => c.As<long>());
-//
-//             callQuery.Query.QueryText.Should().Be(expected);
-//             callQuery.Query.QueryParameters.Should().HaveCount(3);
-//             callQuery.Query.QueryParameters.Should().ContainKeys("p0", "p1", "p2");
-//         }
-//
-//         [Fact]
-//         public void Call_SubQueriesAsLambdaWithParametersPriorAndAfterToCall_WholeWord()
-//         {
-//             const string expected = @"MATCH (x)
-// WHERE (x.Id = $p0)
-// CALL { MATCH (n)
-// WHERE (n.Id = $p1)
-// AND (n.Something = $p2)
-// RETURN count(n) AS c }
-// WHERE (y.Id = $p3)
-// RETURN c";
-//
-//             var client = GraphClient_30;
-//             var query = new CypherFluentQuery(client)
-//                 .Match("(n)")
-//                 .Where("(n.Id = $p0)")
-//                 .AndWhere("(n.Something = $p1)")
-//                 .WithParam("p0", 1)
-//                 .WithParam("p1", 2)
-//                 .Return(n => new {c = n.Count()});
-//             
-//             var callQuery = new CypherFluentQuery(client)
-//                 .Match("(x)")
-//                 .Where((IdClass x) => x.Id == 1)
-//                 .Call(() => query)
-//                 .Where((IdClass y) => y.Id == 1)
-//                 .Return(c => c.As<long>());
-//
-//             callQuery.Query.QueryText.Should().Be(expected);
-//             callQuery.Query.QueryParameters.Should().HaveCount(4);
-//             callQuery.Query.QueryParameters.Should().ContainKeys("p0", "p1", "p2", "p3");
-//         }
-//         private class IdClass {public int Id { get;set; }}
+
+         [Fact]
+         public void Call_SubQueriesAsLambdaWithParameters()
+         {
+             const string expected = "CALL { MATCH (n)\r\nWHERE (n.Id = $p0)\r\nRETURN count(n) AS c }\r\nRETURN c";
+
+             var client = GraphClient_30;
+             var query = new CypherFluentQuery(client)
+                 .Match("(n)")
+                 .Where((Foo n) => n.Id == 1)
+                 .Return(n => new {c = n.Count()});
+             
+             var callQuery = new CypherFluentQuery(client)
+                 .Call(() => query)
+                 .Return(c => c.As<long>());
+
+             callQuery.Query.QueryText.Should().Be(expected);
+             callQuery.Query.QueryParameters.Should().HaveCount(1);
+         }
+
+         [Fact]
+         public void Call_SubQueriesAsLambdaWithParametersPriorToCall()
+         {
+             const string expected = "MATCH (x)\r\nWHERE (x.Id = $p0)\r\nCALL { MATCH (n)\r\nWHERE (n.Id = $p1)\r\nRETURN count(n) AS c }\r\nRETURN c";
+
+             var client = GraphClient_30;
+             var query = new CypherFluentQuery(client)
+                 .Match("(n)")
+                 .Where((Foo n) => n.Id == 1)
+                 .Return(n => new {c = n.Count()});
+             
+             var callQuery = new CypherFluentQuery(client)
+                 .Match("(x)")
+                 .Where((Foo x) => x.Id == 2)
+                 .Call(() => query)
+                 .Return(c => c.As<long>());
+
+             callQuery.Query.QueryText.Should().Be(expected);
+             callQuery.Query.QueryParameters.Should().HaveCount(2);
+         }
+
+         [Fact]
+         public void Call_SubQueriesAsLambdaWithParametersAfterToCall()
+         {
+             const string expected = "CALL { MATCH (n)\r\nWHERE (n.Id = $p0)\r\nRETURN count(n) AS c }\r\nMATCH (x)\r\nWHERE (x.Id = $p1)\r\nRETURN c";
+
+             var client = GraphClient_30;
+             var query = new CypherFluentQuery(client)
+                 .Match("(n)")
+                 .Where((Foo n) => n.Id == 1)
+                 .Return(n => new {c = n.Count()});
+             
+             var callQuery = new CypherFluentQuery(client)
+                 .Call(() => query)
+                 .Match($"(x)")
+                 .Where((Foo x) => x.Id == 2)
+                 .Return(c => c.As<long>());
+
+             callQuery.Query.QueryText.Should().Be(expected);
+             callQuery.Query.QueryParameters.Should().HaveCount(2);
+         }
+
+         [Fact]
+         public void Call_SubQueriesAsLambdaWithParametersPriorToCall_WholeWord()
+         {
+             const string expected = "MATCH (x)\r\nWHERE (x.Id = $p0)\r\nCALL { MATCH (n)\r\nWHERE (n.Id = $p1)\r\nAND (n.Something = $p2)\r\nRETURN count(n) AS c }\r\nRETURN c";
+
+             var client = GraphClient_30;
+             var query = new CypherFluentQuery(client)
+                 .Match("(n)")
+                 .Where("(n.Id = $p0)")
+                 .AndWhere("(n.Something = $p1)")
+                 .WithParam("p0", 1)
+                 .WithParam("p1", 2)
+                 .Return(n => new {c = n.Count()});
+             
+             var callQuery = new CypherFluentQuery(client)
+                 .Match("(x)")
+                 .Where("(x.Id = $p0)")
+                 .WithParam("p0", 3)
+                 .Call(() => query)
+                 .Return(c => c.As<long>());
+
+             callQuery.Query.QueryText.Should().Be(expected);
+             callQuery.Query.QueryParameters.Should().HaveCount(3);
+             callQuery.Query.QueryParameters.Should().ContainKeys("p0", "p1", "p2");
+         }
+
+         [Fact]
+         public void Call_SubQueriesAsLambdaWithParametersPriorAndAfterToCall_WholeWord()
+         {
+             const string expected = "MATCH (x)\r\nWHERE (x.Id = $p0)\r\nCALL { MATCH (n)\r\nWHERE (n.Id = $p1)\r\nAND (n.Something = $p2)\r\nRETURN count(n) AS c }\r\nWHERE (y.Id = $p3)\r\nRETURN c";
+
+             var client = GraphClient_30;
+             var query = new CypherFluentQuery(client)
+                 .Match("(n)")
+                 .Where("(n.Id = $p0)")
+                 .AndWhere("(n.Something = $p1)")
+                 .WithParam("p0", 1)
+                 .WithParam("p1", 2)
+                 .Return(n => new {c = n.Count()});
+             
+             var callQuery = new CypherFluentQuery(client)
+                 .Match("(x)")
+                 .Where((IdClass x) => x.Id == 1)
+                 .Call(() => query)
+                 .Where((IdClass y) => y.Id == 1)
+                 .Return(c => c.As<long>());
+
+             callQuery.Query.QueryText.Should().Be(expected);
+             callQuery.Query.QueryParameters.Should().HaveCount(4);
+             callQuery.Query.QueryParameters.Should().ContainKeys("p0", "p1", "p2", "p3");
+         }
+         private class IdClass {public int Id { get;set; }}
     }
 }
